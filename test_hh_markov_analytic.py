e22087940b6d6abb3a7bcba868689004fa7f0202
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2020-10-16 15:31:46 +0100

    Change test file so it can be run by pytest

--- test_hh_markov_analytic.py
+++ test_hh_markov_analytic.py
@@ -4,7 +4,7 @@ import sympy as sp
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
-def main():
+def compare_solutions(max_t=10):
     #Setup the model with an example parameter set and constant voltage
     # params = [1, 1, 1, 1, 1, 1, 1, 0.1524]
     params = [2.26E-04, 0.0699, 3.45E-05, 0.05462, 0.0873, 8.92E-03, 5.150E-3, 0.03158, 0.1524]
@@ -18,23 +18,25 @@ def main():
     C2 = -k3/(k3+k4)
 
     # Calculate the current at time t analytically
-    t = 5
+    t = np.linspace(0, max_t, 10000)
     x1 = C1*np.exp(t*(-k1-k2)) + k1/(k1+k2)
     x2 = C2*np.exp(t*(-k3-k4)) + k3/(k3+k4)
 
-    analyticI = model.calculateCurrent((1-x1)*(1-x2), x1*(1-x2), x1*x2)
+    analyticI = model.calculateCurrent((1-x1[-1])*(1-x2[-1]), x1[-1]*(1-x2[-1]), x1[-1]*x2[-1])
 
     # Run the model numerically
     numerical_model = ChannelModel(params, lambda t : -80)
-    solution = integrate.solve_ivp(model.getDerivatives, [0,t], model.getStates(0))
+    solution = integrate.solve_ivp(model.getDerivatives, [0,t[-1]], model.getStates(0))
     y = solution.y
 
     #Get the current for the final timestep (at t)
     numericalI = model.calculateCurrent(y[0,-1], y[1, -1], y[2,-1])
 
     print(numericalI, analyticI)
-    assert((numericalI - analyticI)/analyticI < 0.1)
+    if not analyticI == 0:
+        assert(np.abs(numericalI - analyticI)/analyticI < 0.1)
 
 
-if __name__ == '__main__':
-    main()
+def test_solutions_agree():
+    for t in np.linspace(0,1000,100):
+        compare_solutions(t)
