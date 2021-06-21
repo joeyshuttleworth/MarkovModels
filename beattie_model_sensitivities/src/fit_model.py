Fix tests, fix FIM computation

beattie_model_sensitivities/src/fit_model.py
@@ -6,6 +6,7 @@ import pints
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import scipy
 import scipy.integrate as integrate
 import symengine as se
 import matplotlib.pyplot as plt
@@ -14,7 +15,6 @@ import argparse
 
 from   settings import Params
 from   sensitivity_equations import GetSensitivityEquations, CreateSymbols
-from   scipy.stats import chi2
 
 class PintsWrapper(pints.ForwardModelS1):
 
@@ -116,6 +116,10 @@ def main():
     parser.add_argument("-o", "--output", type=str, default="output", help="The directory to output figures and data to")
     args = parser.parse_args()
 
+    data  = pd.read_csv(args.data_file_path, delim_whitespace=True)
+
+    print("outputting to {}".format(args.output))
+
     # Create output directory
     if not os.path.exists(args.output):
         os.mkdir(args.output)
@@ -128,7 +132,6 @@ def main():
 
     skip = int(par.timestep/0.1)
 
-    data  = pd.read_csv(args.data_file_path, delim_whitespace=True)
     dat = extract_times(data.values, indices_to_use, skip)
     times=dat[:,0]
     values=dat[:,1]
@@ -141,22 +144,46 @@ def main():
     boundaries  = Boundaries()
     x0 = np.array([0.1]*9)
     # found_parameters, found_value = pints.optimise(error, starting_parameters, boundaries=boundaries)
-    found_parameters = [1.68482689e-05, -1.19434719e-01, 1.72795030e-05, 3.34388390e-01, 4.42337711e-01,  1.56335228e-01, 1.76629398e-03, -6.59798458e-01, 2.45905373e-01]
-    found_value = 166.01906668991023
-
-    print("finished! found parameters : {} ".format(found_parameters, found_value))
+    # found_parameters = np.array([2.26E-04, 0.0699, 3.45E-05, 0.05462, 0.0873, 8.92E-03, 5.150E-3, 0.03158, 0.1524])
+    # print("finished! found parameters : {} ".format(found_parameters, found_value))
+    found_parameters = np.array([1.87451202e-03, 1.36254787e-02, 1.68324276e-05, 8.77532812e-02, 5.67114947e-02, 2.66069061e-02, 1.21159939e-03, 7.96959925e-03, 5.49219181e-02])
+    # found_value=100
 
     # Find error sensitivities
     funcs = model.funcs
-    sens = funcs.SimulateForwardModelSensitivities(found_parameters)[1]
+    current, sens = funcs.SimulateForwardModelSensitivities(found_parameters)
+    sens = sens * found_parameters[:, None]
+
+    plt.plot(times, current)
+    for vec in sens:
+        plt.plot(times, vec)
+
+    if args.plot:
+        plt.show()
+    else:
+        plt.savefig(os.path.join(args.output, "maximal_likelihood_sensitivities"))
+
+    # Compute the Fischer information matrix
     FIM = sens @ sens.T
     cov = FIM**-1
     eigvals = np.linalg.eigvals(FIM)
-    cov_ellipse(cov[0:2,0:2])
-    print('Eigenvalues of FIM:\n{}'.format(eigvals))
-    print("Covariance matrix is {}".format(cov))
+    for i in range(0, par.n_params):
+        for j in range(i+1, par.n_params):
+            parameters_to_view = np.array([i,j])
+            sub_cov = cov[parameters_to_view[:,None], parameters_to_view]
+            eigen_val, eigen_vec = np.linalg.eigh(sub_cov)
+            eigen_val=eigen_val.real
+            if eigen_val[0] > 0 and eigen_val[1] > 0:
+                print("COV_{},{} : well defined".format(i, j))
+                cov_ellipse(sub_cov, i, j)
+            else:
+                print("COV_{},{} : negative eigenvalue".format(i,j))
 
-def cov_ellipse(cov, q=0.95, nsig=None, **kwargs):
+
+    print('Eigenvalues of FIM:\n{}'.format(eigvals))
+    print("Covariance matrix is: \n{}".format(cov))
+
+def cov_ellipse(cov, i, j, q=None, nsig=1, **kwargs):
     """
     Parameters
     ----------
@@ -178,27 +205,33 @@ def cov_ellipse(cov, q=0.95, nsig=None, **kwargs):
     for the ellipse.
     """
 
-    if q is not None
+    if q is not None:
         q = np.asarray(q)
     elif nsig is not None:
-        q = 2 * norm.cdf(nsig) - 1
+        q = 2 * scipy.stats.norm.cdf(nsig) - 1
     else:
         raise ValueError('One of `q` and `nsig` should be specified.')
-    r2 = chi2.ppf(q, 2)
+    r2 = scipy.stats.chi2.ppf(q, 2)
 
     val, vec = np.linalg.eigh(cov)
-    print(val)
     width, height = 2 * np.sqrt(val[:, None] * r2)
     rotation = np.degrees(np.arctan2(*vec[::-1, 0]))
 
+    print("width, height, rotation = {}, {}, {}".format(width, height,rotation))
+
     fig = plt.figure(0)
-    e = matplotlib.patches.Ellipse([0,0], width, height, rotation)
+    e = matplotlib.patches.Ellipse([0,0], width[0], height[0], rotation)
     ax = fig.add_subplot(111, aspect='equal')
     ax.add_artist(e)
     e.set_clip_box(ax.bbox)
-    plt.show()
-    print(width, height)
-    return width, height, rotation
+    e.set_facecolor([0.25, 0.25, 0])
+    ax.set_xlim(width[0])
+    ax.set_ylim(height[0])
+
+    if args.plot:
+        plt.show()
+    else:
+        plt.savefig(os.path.join(args.output, "covariance_plot_{}_{}".format(i,j)))
 
 
 if __name__ == "__main__":

beattie_model_sensitivities/src/sensitivity_equations.py
@@ -220,38 +220,25 @@ class GetSensitivityEquations(object):
         """
         return np.array([self.voltage(t) for t, _ in enumerate(self.times)])
 
-    def NormaliseSensitivities(self, S1n, params):
-        """
-        Normalise the sensitivites with regards to the size of the parameter
-
-        This is equivalent to rescaling all of the parameters so that they're
-        equal to 1.
-
-        """
-
-        # Normalise to parameter value
-        for i, param in enumerate(params):
-            S1n[:, i] = S1n[:, i] * param
-        return S1n
-
     def SimulateForwardModelSensitivities(self, p):
         """
         Solve the model for a given set of parameters
 
         Returns the state variables and current sensitivities at every timestep
 
+        Used by pints
         """
         solution = self.solve_drhs_full(p)
 
         # Get the open state sensitivities for each parameter
         index_sensitivities = range(self.par.n_state_vars + self.par.open_state * self.par.n_params, self.par.n_state_vars + (self.par.open_state + 1) *self.par.n_params)
-        sensitivities = solution[:, index_sensitivities].T
+        sensitivities = solution[:, index_sensitivities]
         o = solution[:, self.par.open_state]
         voltages = self.GetVoltage()
         current = self.conductance * o * (voltages - self.par.Erev)
-
-        current_sensitivies = np.stack([self.conductance * o * (voltages - self.par.Erev) * sensitivity for sensitivity in sensitivities])
-        return current, current_sensitivies
+        values = np.stack(np.array([self.conductance * o * (voltages - self.par.Erev) * sensitivity for sensitivity in sensitivities.T]), axis=0)
+        ret_vals = (current, values)
+        return ret_vals
 
     def GetErrorSensitivities(params, data):
         """Solve the model for a given set of parameters

beattie_model_sensitivities/src/simulate_beattie_model_sensitivities.py
@@ -45,8 +45,11 @@ def main():
 
     funcs = GetSensitivityEquations(par, p, y, v, A, B, para, times, sine_wave=args.sine_wave)
 
-    S1 = funcs.SimulateForwardModelSensitivities(para)
-    S1n = funcs.NormaliseSensitivities(S1, para)
+    ret = funcs.SimulateForwardModelSensitivities(para),
+    current = ret[0][0]
+    S1 = ret[0][1]
+
+    S1n = S1 * np.array(para)[:, None]
 
     state_variables = funcs.GetStateVariables(para)
     state_labels = ['C', 'O', 'I', 'IC']
@@ -85,7 +88,7 @@ def main():
 
     H = np.dot(S1n.T, S1n)
     eigvals = np.linalg.eigvals(H)
-    print('Eigenvalues of H:\n{}'.format(eigvals))
+    print('Eigenvalues of H:\n{}'.format(eigvals.real))
 
     fig = plt.figure(figsize=(6, 6), dpi=args.dpi)
     ax = fig.add_subplot(111)

beattie_model_sensitivities/test/test_sensitivity_equations.py
@@ -49,7 +49,7 @@ def test_run_functions():
     B = se.Matrix([k4, 0, k1])
     rhs = np.array(A * y + B)
 
-    data  = pd.read_csv("../data/averaged-data.txt", delim_whitespace=True)
+    data  = pd.read_csv("data/averaged-data.txt", delim_whitespace=True)
     times = data["time"].values
     data  = data["current"].values
 
@@ -57,7 +57,7 @@ def test_run_functions():
 
     # Run Functions
     funcs.SimulateForwardModel(starting_parameters)
-    funcs.SimulateForwardModelSensitivities(starting_parameters, data)
+    funcs.SimulateForwardModelSensitivities(starting_parameters)
 
 
 if __name__ == "__main__":

test_hh_markov_analytic.py
@@ -3,7 +3,7 @@ import numpy as np
 from hh_markov_model import ChannelModel
 import scipy.integrate as integrate
 
-def compare_solutions(params, max_t=10):
+def compare_solutions(params, max_t=50):
     #Setup the model with an example parameter set and constant voltage
 
     assert len(params) == 9, "Parameter vector has the wrong dimensions"
@@ -21,29 +21,30 @@ def compare_solutions(params, max_t=10):
     C2 = -k3/(k3+k4)
 
     # Calculate the current at time t analytically
-    t = np.linspace(0, max_t, 10000)
+    t = np.linspace(0, max_t, 100)
     x1 = C1*np.exp(t*(-k1-k2)) + k1/(k1+k2)
     x2 = C2*np.exp(t*(-k3-k4)) + k3/(k3+k4)
 
-    analyticI = model.calculateCurrent((1-x1[-1])*(1-x2[-1]), x1[-1]*(1-x2[-1]), x1[-1]*x2[-1])
+    analyticI = model.calculateCurrent(x1[-1] * x2[-1])
 
     # Run the model numerically
     numerical_model = ChannelModel(params, lambda t : -80)
-    solution = integrate.solve_ivp(model.getDerivatives, [0,t[-1]], model.getStates(0))
+    solution = integrate.solve_ivp(model.getDerivatives, [0,t[-1]], model.getStates(0), rtol=1e-10, atol=1e-10)
     y = solution.y
 
     #Get the current for the final timestep (at t)
-    numericalI = model.calculateCurrent(y[0,-1], y[1, -1], y[2,-1])
+    numericalI = model.calculateCurrent(y[1,-1])
 
     print(numericalI, analyticI)
     if not analyticI == 0:
-        assert np.abs(numericalI - analyticI)/analyticI < 0.1, "Solutions don't match"
+        #TODO Test this better
+        assert np.abs(numericalI - analyticI) < 0.0001, "Solutions don't match"
 
 
 def test_solutions_agree():
     params = [2.26E-04, 0.0699, 3.45E-05, 0.05462, 0.0873, 8.92E-03, 5.150E-3, 0.03158, 0.1524]
-    for t in np.linspace(0, 1000, 101):
-        compare_solutions(params, 1000)
+    for t in np.linspace(0, 200, 10):
+        compare_solutions(params, t)
 
 
 def test_nonsensical_parameters():