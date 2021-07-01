0099946de9effde812bf0c586832757063a1b265
Joseph Shuttleworth <pmyjgs@godiva.maths.nottingham.ac.uk>
2021-07-01 15:33:40 +0100

    Fix gpce plots

--- gpce.py
+++ gpce.py
@@ -45,24 +45,27 @@ def do_gpce(forward_problem, starting_parameters, first_V, second_V, distributio
     print(expected, std)
     print(foo_approx)
     if plot:
-        xs = np.linspace(distribution.mom(1)-math.sqrt(distribution.mom(2))*3,distribution.mom(1) + math.sqrt(distribution.mom(2))*3, 50)
-        # plt.plot(xs, distribution.pdf(xs))
-        plt.xlabel("p8")
-        print(xs)
-        plt.axvline(distribution.mom(1), linestyle="--", label="mean parameter value")
-        plt.plot(xs, [foo_approx(x)[0].sum() for x in xs], label="gpce approximation" ,color="blue")
-        plt.plot(xs, [forward_problem(x)[0] for x in xs], label="true value", color="red")
-        plt.ylabel("current /nA")
-        plt.legend()
-        plt.savefig("normal_pdf_{}mV_{}mV.pdf".format(first_V, second_V))
-        plt.clf()
+        xs = np.linspace(distribution.mom(1)-math.sqrt(distribution.mom(2))*5,distribution.mom(1) + math.sqrt(distribution.mom(2))*5, 50)
+        fig, ax = plt.subplots()
+        ax.set_xlabel("p8")
+        ax.axvline(distribution.mom(1), linestyle="--", label="mean parameter value")
+        ax.plot(xs, [foo_approx(x)[0].sum() for x in xs], label="gpce approximation" ,color="blue")
+        ax.plot(xs, [forward_problem(x)[0] for x in xs], label="true value", color="red")
+        ax.set_ylabel("current /nA")
 
-        plt.fill_between(coordinates, expected-std, expected+std, alpha=0.3)
-        plt.plot(coordinates, expected)
-        plt.savefig("pseudo_spectral_plot_{}mV_{}mV.pdf".format(first_V, second_V))
-        plt.xlabel("time /ms")
-        plt.ylabel("current /nA")
-        plt.clf()
+        ax2 = ax.twinx()
+        x2s = np.linspace(xs[0], xs[-1], 10000)
+        ax2.plot(xs, distribution.pdf(xs), "--", label="probability density", color="green")
+
+        ax.legend()
+        fig.savefig("normal_pdf_{}mV_{}mV.pdf".format(first_V, second_V))
+
+        fig, ax = plt.subplots()
+        ax.fill_between(coordinates, expected-std, expected+std, alpha=0.3)
+        ax.plot(coordinates, expected)
+        ax.set_xlabel("time /ms")
+        ax.set_ylabel("current /nA")
+        fig.savefig("pseudo_spectral_plot_{}mV_{}mV.pdf".format(first_V, second_V))
         print("plotted")
 
     return expected, std
@@ -86,7 +89,7 @@ if __name__ == "__main__":
     coordinates = np.linspace(20, 500, 100)
     starting_parameters = [3.87068845e-04, 5.88028759e-02, 6.46971727e-05, 4.87408447e-02, 8.03073893e-02, 7.36295506e-03, 5.32908518e-03, 3.32254316e-02, 6.56614672e-02]
 
-    distribution = chaospy.Normal(starting_parameters[index_of_parameter], starting_parameters[index_of_parameter]/2)
+    distribution = chaospy.Normal(starting_parameters[index_of_parameter], starting_parameters[index_of_parameter])
     forward_problem = get_forward_problem(starting_parameters, index_of_parameter, coordinates, first_V, second_V)
     mean, std = do_gpce(forward_problem, starting_parameters, first_V, second_V, distribution, quad_order=50, poly_order=10, plot=True)
 
