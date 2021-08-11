a4444efa992cfd9d233b941f82cf518fbf7dbadd
Joseph Shuttleworth <pmyjgs@godiva.maths.nottingham.ac.uk>
2021-08-11 15:24:55 +0100

    Rearrange directory structure. Add unittest

---
+++ test/test_sensitivity_equations.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import os
+import unittest
+import pints
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy.integrate as integrate
+import symengine as se
+import argparse
+
+import argparse
+import matplotlib.pyplot as plt
+
+from MarkovModels import *
+
+
+
+class Test(unittest.TestCase):
+
+    def test_foo():
+        assert(false)
+
+    def test_run_functions():
+        # Check input arguments
+        parser = argparse.ArgumentParser(
+            description='Plot sensitivities of the Beattie model')
+        parser.add_argument("-s", "--sine_wave", action='store_true', help="whether or not to use sine wave protocol",
+            default=False)
+        parser.add_argument("-p", "--plot", action='store_true', help="whether to plot figures or just save",
+            default=False)
+        parser.add_argument("--dpi", type=int, default=100, help="what DPI to use for figures")
+        args = parser.parse_args()
+
+        par = Params()
+        starting_parameters = [2.26E-04, 0.0699, 3.45E-05, 0.05462, 0.0873, 8.92E-03, 5.150E-3, 0.03158, 0.1524]
+        # Create symbols for symbolic functions
+        p, y, v = CreateSymbols(par)
+
+        # Choose starting parameters (from J Physiol paper)
+        para = [2.26E-04, 0.0699, 3.45E-05, 0.05462, 0.0873, 8.92E-03, 5.150E-3, 0.03158, 0.1524]
+
+        # Create symbols for symbolic functions
+        p, y, v = CreateSymbols(par)
+
+        # Define system equations and initial conditions
+        k1 = p[0] * se.exp(p[1] * v)
+        k2 = p[2] * se.exp(-p[3] * v)
+        k3 = p[4] * se.exp(p[5] * v)
+        k4 = p[6] * se.exp(-p[7] * v)
+
+        # Write in matrix form taking y = ([C], [O], [I])^T
+        A = se.Matrix([[-k1 - k3 - k4, k2 -  k4, -k4], [k1, -k2 - k3, k4], [-k1, k3 - k1, -k2 - k4 - k1]])
+        B = se.Matrix([k4, 0, k1])
+        rhs = np.array(A * y + B)
+
+        data  = pd.read_csv("data/averaged-data.txt", delim_whitespace=True)
+        times = data["time"].values
+        data  = data["current"].values
+
+        funcs = GetSensitivityEquations(par, p, y, v, A, B, para, times, sine_wave=args.sine_wave)
+
+        # Run Functions
+        funcs.SimulateForwardModel(starting_parameters)
+        funcs.SimulateForwardModelSensitivities(starting_parameters)
+
+
+    def test_sens_limits():
+        par = Params()
+        p, y, v = CreateSymbols(par)
+        reversal_potential = par.Erev
+        para = np.array([2.07, 7.17E1, 3.44E-2, 6.18E1, 4.18E2, 2.58E1, 4.75E1, 2.51E1, 3.33E1])
+        para = para*1E-3
+
+        # Define system equations and initial conditions
+        k1 = p[0] * se.exp(p[1] * v)
+        k2 = p[2] * se.exp(-p[3] * v)
+        k3 = p[4] * se.exp(p[5] * v)
+        k4 = p[6] * se.exp(-p[7] * v)
+
+        current_limit = (p[-1]*(par.holding_potential - reversal_potential) * k1/(k1+k2) * k4/(k3+k4)).subs(v, par.holding_potential)
+        print("{} Current limit computed as {}".format(__file__, current_limit.subs(p, para).evalf()))
+
+        sens_inf = [float(se.diff(current_limit, p[j]).subs(p, para).evalf()) for j in range(0, par.n_params)]
+        print("{} sens_inf calculated as {}".format(__file__, sens_inf))
+
+
+        k = se.symbols('k1, k2, k3, k4')
+
+        # Notation is consistent between the two papers
+        A = se.Matrix([[-k1 - k3 - k4, k2 -  k4, -k4], [k1, -k2 - k3, k4], [-k1, k3 - k1, -k2 - k4 - k1]])
+        B = se.Matrix([k4, 0, k1])
+
+
+        # Use results from HH equations
+        current_limit = (p[-1]*(par.holding_potential - reversal_potential) * k1/(k1+k2) * k4/(k3+k4)).subs(v, par.holding_potential)
+
+        funcs = GetSensitivityEquations(par, p, y, v, A, B, para, [0])
+        sens_inf = [float(se.diff(current_limit, p[j]).subs(p, para).evalf()) for j in range(0, par.n_params)]
+
+        sens=funcs.SimulateForwardModelSensitivities(para)[1]
+
+        # Check sens = sens_inf
+        error = np.abs(sens_inf - sens)
+
+        equal = np.all(error<1e-10)
+        assert(equal)
+        return
+
+if __name__ == "__main__":
+    unittest.main()
