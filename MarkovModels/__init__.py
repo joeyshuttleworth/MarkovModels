ad254b315dd79d29382a2d22001e442da610cc93
Joseph Shuttleworth <pmyjgs@godiva.maths.nottingham.ac.uk>
2021-09-21 15:57:52 +0100

    Remove DynamicMarkovModel

--- MarkovModels/__init__.py
+++ MarkovModels/__init__.py
@@ -1,6 +1,5 @@
 from . MarkovModel import MarkovModel
 from . MarkovChain import MarkovChain
-from . DynamicMarkovChain import DynamicMarkovChain
 from . BeattieModel import BeattieModel
 from . M10Model import M10Model
 from . common import *
