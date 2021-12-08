8ca9a0cc45ee7d074a50a9a2cdb0e9245df638b7
Joseph <pmyjgs@nottingham.ac.uk>
2021-12-08 15:20:45 +0000

    Use multiprocessing in simulate_protocols

--- MarkovModels/simulate_protocols.py
+++ MarkovModels/simulate_protocols.py
@@ -6,6 +6,7 @@ import common
 import numpy as np
 import logging
 import matplotlib.pyplot as plt
+import multiprocessing
 
 def simulate_protocol(model, name, output_dir, fig, axs):
     print(f"Plotting {name}")
@@ -15,7 +16,7 @@ def simulate_protocol(model, name, output_dir, fig, axs):
     param_labels = ['S(p' + str(i + 1) + ',t)' for i in range(model.n_params)]
     state_occupancies = model.GetStateVariables()
 
-    interp_voltage, _, _, _, _ = common.get_protocol_from_csv(name, directory=common.get_protocol_directory())
+    interp_voltage, _, _, _ = common.get_protocol_from_csv(name, directory=common.get_protocol_directory())
     axs[0].plot(model.times, model.GetVoltage(), label='generated voltage function')
     axs[0].plot(model.times, [interp_voltage(t) for t in model.times], label='actual voltage')
     axs[0].plot()
@@ -45,24 +46,6 @@ def simulate_protocol(model, name, output_dir, fig, axs):
     for ax in axs:
         ax.cla()
 
-    S1n = S1 * model.get_default_parameters()
-    H = np.dot(S1n.T, S1n)
-
-    try:
-        eigvals = np.linalg.eigvals(H)
-        # Plot the eigenvalues of H, shows the condition of H
-        fig = plt.figure(figsize=(14, 12))
-        ax = fig.add_subplot(111)
-        for i in eigvals:
-            ax.axhline(y=i, xmin=0.25, xmax=0.75)
-            ax.set_yscale('log')
-            ax.set_xticks([])
-            ax.grid(True)
-    except np.linalg.LinAlgError as err:
-        pass
-    fig.savefig(os.path.join(output_dir, f"{name}_H_eigenvalues"))
-
-
 def main():
     parser = argparse.ArgumentParser(description="Plot output from different protocols")
     output_dir = os.path.join('output', 'simulate_protocols')
@@ -73,19 +56,31 @@ def main():
     fig = plt.figure(figsize=(14, 12))
     axs = fig.subplots(4)
 
+    tasks = []
     for fname in os.listdir(common.get_protocol_directory()):
         fname, ext = os.path.splitext(fname)
         if fname[0] == '#':
             continue
+        tasks.append(fname)
+
+
+    global func
+    def func(protocol_name):
         if ext != ".csv":
             logging.warning(f"Using file with extension {ext}")
+
         protocol, t_start, t_end, t_step = common.get_protocol(os.path.splitext(fname)[0])
         times = np.linspace(t_start, t_end, int((t_end-t_start)/t_step))
         model = BeattieModel(protocol, times)
         # model.default_parameters = np.array([2.07E-3, 7.17E-2, 3.44E-5, 6.18E-2, 4.18E-1, 2.58E-2, 4.75E-2, 2.51E-2, 3.33E-2])
         model.default_parameters = np.array([0.00023215680795174809,0.07422110165735675,2.477501557744992e-05,0.04414799725791213,0.11023652619943154,0.015996823969951217,0.015877336172564104,0.027816696279347616,49.70368237942998])
         model.Erev = common.calculate_reversal_potential(298, K_out=130, K_in=5)
-        simulate_protocol(model, fname, output_dir, fig, axs)
+        simulate_protocol(model, protocol_name, output_dir, fig, axs)
+
+    pool = multiprocessing.Pool(processes=os.cpu_count())
+    pool.map(func, tasks)
+
+
 
 
 if __name__ == "__main__":
