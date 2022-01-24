c022ed56e7b569441e30b4a78181268288caa098
Joseph <pmyjgs@nottingham.ac.uk>
2022-01-24 11:54:32 +0000

    Modify removal durations

--- plot_criteria.py
+++ plot_criteria.py
@@ -42,7 +42,7 @@ def main():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    spike_removal_durations = np.unique(list(np.linspace(0, 5, 6)) + list(np.linspace(5, 251, 20)))
+    spike_removal_durations = np.unique(list(np.linspace(0, 10, 11)) + list(np.linspace(15, 255, 20)))
 
     params = np.array([2.07E-3, 7.17E-2, 3.44E-5, 6.18E-2, 4.18E-1, 2.58E-2,
                        4.75E-2, 2.51E-2, 3.33E-2])
