118dc02ec626804c518fdb2b2126d538531fa6f6
Joseph <pmyjgs@nottingham.ac.uk>
2022-01-25 11:17:04 +0000

    Sample from mcmc chains more sensibly

--- plot_criteria.py
+++ plot_criteria.py
@@ -222,8 +222,7 @@ def main():
     indices = np.unique(np.array(list(range(len(times)))[::50] + list(spike_indices)))
 
     for j, mcmc_sample in enumerate(mcmc_samples):
-        for i in range(min(mcmc_sample.shape[1], 100)):
-            sample = mcmc_sample[0, -i, :]
+        for sample in np.random.choice(mcmc_sample[0], 1000):
             trajectory = solver(sample, times)
             traj_ax.plot(times[indices], trajectory[indices], color='grey', alpha=.3)
         traj_fig.savefig(os.path.join(output_dir, f"{j}_mcmc_trajectories.png"))
