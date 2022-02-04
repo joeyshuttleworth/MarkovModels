4c0e6d636fd9939ff3ba4b3098ea60ad9b8fc6cb
Joseph <pmyjgs@nottingham.ac.uk>
2022-02-04 13:27:04 +0000

    Revert change to removal durations

--- plot_criteria.py
+++ plot_criteria.py
@@ -45,8 +45,6 @@ def main():
         os.makedirs(output_dir)
 
     spike_removal_durations = np.unique(list(np.linspace(0, 10, 11)) + list(np.linspace(10, 100, 30))[1:])
-    spike_removal_durations = [0, 10]
-
     params = np.array([2.07E-3, 7.17E-2, 3.44E-5, 6.18E-2, 4.18E-1, 2.58E-2,
                        4.75E-2, 2.51E-2, 3.33E-2])
 
