081724c471b0d89ad6969e675b7e85bd0fe0e461
Joseph <pmyjgs@nottingham.ac.uk>
2022-02-07 17:56:39 +0000

    Reverse colourramp log plot

--- plot_fitting_validation_heatmap.py
+++ plot_fitting_validation_heatmap.py
@@ -2,21 +2,21 @@
 
 from MarkovModels import common
 import os
-import logging
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import pints
-import pints.plot
-import uuid
 import argparse
 import matplotlib.pyplot as plt
+
 
 def main():
     description = ""
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("input_file", help="CSV file listing model errors for each cell and protocol")
-    parser.add_argument("--output_dir", "-o", help="Directory to output plots to. By default a new directory will be generated", default=None)
+    parser.add_argument("--output_dir", "-o", help="Directory to output plots to.\
+    By default a new directory will be generated", default=None)
+
+    parser.add_argument("--vmax", "-m", default=None)
 
     args = parser.parse_args()
     df = pd.read_csv(args.input_file)
@@ -28,14 +28,20 @@ def main():
     # Iterate over wells for heatmap
     for well in df['well'].unique():
         ax = fig.subplots()
+        df['log RMSE'] = np.log(df['RMSE'])
         sub_df = df[df.well == well].pivot(index='fitting_protocol', columns='validation_protocol',
-                                           values='RMSE')
-        sns.heatmap(sub_df, ax=ax, cbar_kws={'label': 'RMSE'}, vmin=0)
+                                           values='log RMSE')
+        cmap = sns.cm.rocket_r
+        sns.heatmap(sub_df, ax=ax, cbar_kws={'label': 'log RMSE'}, vmin=None, vmax=args.vmax, cmap=cmap)
 
         ax.set_title(f"well {well}")
+        ax.set_ylabel("Fitting protocol")
+        ax.set_xlabel("Validation protocol")
 
+        fig.tight_layout()
         fig.savefig(os.path.join(output_dir, f"{well}_fit_predict_heatmap.png"))
         fig.clf()
+
 
 if __name__ == "__main__":
     main()
