be5324f41f825e2e9209896a5e7dde12002b79ca
Joseph <pmyjgs@nottingham.ac.uk>
2022-03-04 17:44:00 +0000

    Change default output dirname

--- MarkovModels/common.py
+++ MarkovModels/common.py
@@ -720,7 +720,7 @@ def get_git_revision_hash() -> str:
 def setup_output_directory(dirname: str = None, subdir_name: str = None):
 
     if dirname is None:
-        dirname = os.path.join("output", f"output-{uuid.uuid4()}")
+        dirname = os.path.join("output", f"{subdir_name}-{uuid.uuid4()}")
 
     if subdir_name is not None:
         dirname = os.path.join(dirname, subdir_name)
