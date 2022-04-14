f6d6b114f5620f32ebc7dfdd8b3cbc22094bf29c
Joseph <pmyjgs@nottingham.ac.uk>
2022-04-14 14:52:57 +0100

    Print sigma2

--- fit_all_wells_and_protocols.py
+++ fit_all_wells_and_protocols.py
@@ -57,7 +57,7 @@ def mcmc_func(protocol, well, model_class, initial_params):
         initial_params = model.get_default_parameters()
 
     sigma2 = np.std(data[10:50])**2
-    print(sigma2)
+    print("sigma2 is ", sigma2)
 
     spike_times, spike_indices = common.detect_spikes(times, voltages, threshold=10)
 
