d346cbbbca9bdf2ab461e306841b82a453d340cf
Joseph <pmyjgs@nottingham.ac.uk>
2022-05-11 08:59:20 +0100

    Add options to simulate protocol

--- simulate_protocols.py
+++ simulate_protocols.py
@@ -9,61 +9,51 @@ import logging
 import matplotlib.pyplot as plt
 import pandas as pd
 
+global params
+params = np.array([2.07E-3, 7.17E-2, 3.44E-5, 6.18E-2, 4.18E-1, 2.58E-2,
+                   4.75E-2, 2.51E-2, 3.33E-2])
+
+
 def simulate_protocol(model, name, output_dir, params=None):
-    fig = plt.figure(figsize=(14, 12))
-    axs = fig.subplots(4)
+    if args.poster_plot:
+        figsize = (6, 6)
+        plt.axis('off')
+        transparent = True
+    else:
+        figsize = (12, 9)
+        transparent = False
 
     print(f"Plotting {name} to {output_dir}")
 
-    model.set_tolerances(1e-7, 1e-9)
-
-    current, S1 = model.SimulateForwardModelSensitivities()
-    S1n = S1 * np.array(model.get_default_parameters())[None, :]
-
-    param_labels = ['S(p' + str(i + 1) + ',t)' for i in range(model.n_params)]
-    state_occupancies = model.GetStateVariables()
-
-    axs[0].plot(model.times, model.GetVoltage(), label='generated voltage function')
-    axs[0].plot()
-    axs[0].grid(True)
-    axs[0].set_xticklabels([])
-    axs[0].set_ylabel('Voltage (mV)')
-
-    axs[1].plot(model.times, current)
-    axs[1].grid(True)
-    axs[1].set_xticklabels([])
-    axs[1].set_ylabel('Current (nA)')
-    axs[2].plot(model.times, state_occupancies, label=model.state_labels)
-    axs[2].legend(ncol=4)
-    axs[2].grid(True)
-    axs[2].set_xticklabels([])
-    axs[2].set_ylabel('State occupancy')
-    for i in range(model.n_params - 1):
-        axs[3].plot(model.times, S1n[:, i], label=param_labels[i])
-    axs[3].legend(ncol=3)
-    axs[3].grid(True)
-    axs[3].set_xlabel('Time (ms)')
-    axs[3].set_ylabel('Sensitivities')
-    fig.tight_layout()
-    fig.savefig(os.path.join(output_dir, f"{name}_sensitivities"))
-
-    for ax in axs:
-        ax.cla()
-    plt.close(fig)
-
     # Now plot just the voltage and current
-    fig = plt.figure(figsize=(14, 12))
+    fig = plt.figure(figsize=figsize, dpi=1000)
     axs = fig.subplots(2)
 
-    axs[0].set_title(name)
+    current = model.SimulateForwardModel()
+    observations = current + np.random.normal(0, args.noise, current.shape)
 
-    axs[0].plot(model.times, model.GetVoltage())
-    axs[1].plot(model.times, model.SimulateForwardModel())
-    axs[1].set_xlabel('time /ms')
-    axs[1].set_ylabel('current /nA')
-    axs[0].set_ylabel('voltage / mV')
+    axs[0].plot(model.times, model.GetVoltage(), color='black')
+    axs[1].plot(model.times, observations, color='grey', alpha=0.5)
+    axs[1].plot(model.times, current, color='green')
 
-    fig.savefig(os.path.join(output_dir, f"{name}_protocol"))
+    if not args.poster_plot:
+        axs[0].set_title(name)
+        axs[1].set_xlabel('time /ms')
+        axs[1].set_ylabel('current /nA')
+        axs[0].set_ylabel('voltage / mV')
+
+    bbox_inches = 0 if args.poster_plot else None
+
+    for ax in axs:
+        for side in ['top', 'right', 'bottom', 'left']:
+            ax.spines[side].set_visible(False)
+        ax.get_xaxis().set_ticks([])
+        ax.get_xaxis().set_ticks([])
+        ax.get_yaxis().set_ticks([])
+        ax.get_yaxis().set_ticks([])
+
+    fig.savefig(os.path.join(output_dir, f"{name}_protocol"),
+                bbox_inches=bbox_inches, transparent=transparent)
 
     for ax in axs:
         ax.cla()
@@ -76,15 +66,16 @@ def main():
     parser = argparse.ArgumentParser(description="Plot output from different protocols")
     parser.add_argument("--protocols", "-p", default=[], type=str, nargs='+')
     parser.add_argument("--parameter_file", type=str, default=None)
+    parser.add_argument("-s", "--noise", default=0, type=float)
+    parser.add_argument('--poster_plot', default=False, action='store_true')
 
+    global args
     args = parser.parse_args()
 
     if args.parameter_file is not None:
         params = pd.read_csv(args.parameter_file).values.flatten()
-    else:
-        params = None
 
-    output_dir = os.path.join('output', 'simulate_protocols')
+    output_dir = common.setup_output_directory(None, 'simulate_protocols')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -93,21 +84,17 @@ def main():
         if fname[0] == '#':
             continue
         if fname in args.protocols or len(args.protocols) == 0:
-            func(fname, ext, output_dir, params)
+            func(fname, ext, output_dir)
 
 
-def func(protocol_name, ext, output_dir, params=None):
+def func(protocol_name, ext, output_dir):
     if ext != ".csv":
         logging.warning(f"Using file with extension {ext}")
 
     protocol, t_start, t_end, t_step, protocol_description = common.get_ramp_protocol_from_csv(protocol_name)
     times = np.linspace(t_start, t_end, int((t_end - t_start) / t_step))
-    model = BeattieModel(protocol, times, Erev=common.calculate_reversal_potential(298, K_out=130, K_in=5))
-
-    model.protocol_description = protocol_description
-
-    if params:
-        model.default_parameters = params
+    model = BeattieModel(protocol, times,
+                         protocol_description=protocol_description, parameters=params)
 
     simulate_protocol(model, protocol_name, output_dir)
 
