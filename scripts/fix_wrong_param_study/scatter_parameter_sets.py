42b97966fee8a83737c116f9aeed339d34b4a9b3
Joseph <pmyjgs@nottingham.ac.uk>
2022-11-16 11:07:00 +0000

    Fix scatter lims. Add different markers

--- scripts/fix_wrong_param_study/scatter_parameter_sets.py
+++ scripts/fix_wrong_param_study/scatter_parameter_sets.py
@@ -16,11 +16,15 @@ import seaborn as sns
 import os
 import string
 import re
+import itertools
 
 from matplotlib import rc
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
+
+_markers = itertools.cycle(['o', 'v', '^', '<', '>', 's', 'p', '*', 'h',
+                            'H', 'D', 'd', 'P', 'X'])
 
 protocol_chrono_order = ['staircaseramp1',
                          'sis',
@@ -90,13 +94,15 @@ def main():
     df = df[~df.protocol.isin(args.ignore_protocols)]
     print(df.protocol.unique())
 
+    markers = [next(_markers) for p in df.protocol.unique()]
+
     for i in range(int(len(parameter_labels) / 2)):
         lab1 = parameter_labels[2*i]
         lab2 = parameter_labels[2*i + 1]
 
         if args.same_plot_lims:
-            xlims = np.quantile(df[lab1], [0.01, 0.99])
-            ylims = np.quantile(df[lab2], [0.01, 0.99])
+            xlims = np.quantile(df[lab1], [0, 1])
+            ylims = np.quantile(df[lab2], [0, 1])
             print(xlims, ylims)
 
         for j, val in enumerate(df[args.fix_param].unique()):
@@ -108,7 +114,8 @@ def main():
             sub_df = sub_df.sort_values(by='protocol')
 
             g = sns.scatterplot(data=sub_df, x=lab1, y=lab2, hue='protocol',
-                                legend=True)
+                                style='protocol', legend=True, markers=markers,
+                                linewidth=0.1)
             ax = fig.gca()
 
             if i == 0:
@@ -119,9 +126,8 @@ def main():
             val1 = true_parameters[2*i]
             val2 = true_parameters[2*i + 1]
 
-            ax.axvline(val1, label=f"true {lab1}", linestyle='--')
-            ax.axhline(val2, label=f"true {lab2}",
-                       linestyle='--')
+            ax.axvline(val1, label=f"true {lab1}", linestyle='..')
+            ax.axhline(val2, label=f"true {lab2}", linestyle='..')
 
             if args.same_plot_lims:
                 ax.set_xlim(*xlims)
@@ -132,7 +138,8 @@ def main():
             fig.clf()
 
         # lastly, plot conductance on its own
-        g = sns.stripplot(data=sub_df, y='protocol', hue='protocol', x=parameter_labels[-1])
+        g = sns.stripplot(data=sub_df, y='protocol', hue='protocol',
+                          x=parameter_labels[-1])
         ax = plt.gca()
         ax.legend()
 
