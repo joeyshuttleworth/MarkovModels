2060b9f863559f2bc22626325acc9d11c93eae88
Joseph Shuttleworth <pmyjgs@godiva.maths.nottingham.ac.uk>
2022-11-17 10:29:13 +0000

    Return fitting df

--- scripts/fix_wrong_param_study/fix_wrong_parameter.py
+++ scripts/fix_wrong_param_study/fix_wrong_parameter.py
@@ -224,28 +224,29 @@ def fit_func(model_class_name, dataset_index, fix_param, protocol):
         if pre_score2 < pre_score1:
             params = default_guess
 
-        params, score = common.fit_model(mm, data, fix_parameters=[fix_param],
-                                         repeats=args.repeats,
-                                         max_iterations=args.max_iterations,
-                                         starting_parameters=params,
-                                         solver=solver,
-                                         subset_indices=indices,
-                                         method=args.method)
-
-        if score > min(pre_score1, pre_score2):
-            logging.warning("Fitting resulting in worse score than default/previous parameters."
-                            + f"Refitting with initial parameters\n ({score}"
-                            + f" vs {min(pre_score1, pre_score2)})")
-            score = np.sqrt(score/len(indices))
-
         params, score, fitting_df = common.fit_model(mm, data, fix_parameters=[fix_param],
                                                      repeats=args.repeats,
                                                      max_iterations=args.max_iterations,
+                                                     starting_parameters=params,
                                                      solver=solver,
                                                      subset_indices=indices,
                                                      method=args.method,
                                                      return_fitting_df=True)
 
+        if score > min(pre_score1, pre_score2):
+            logging.warning("Fitting resulting in worse score than default/previous parameters."
+                            + f"Refitting with initial parameters\n ({score}"
+                            + f" vs {min(pre_score1, pre_score2)})")
+
+            params, score, fitting_df = common.fit_model(mm, data, fix_parameters=[fix_param],
+                                                         repeats=args.repeats,
+                                                         max_iterations=args.max_iterations,
+                                                         solver=solver,
+                                                         subset_indices=indices,
+                                                         method=args.method,
+                                                         return_fitting_df=True)
+
+        score = np.sqrt(score/len(indices))
         params[fix_param] = fix_param_val
 
         fit_ax.plot(times, data, color='grey')
