cead31831d9effa435dd5b6f1f6811edcd6e632c
Joseph <pmyjgs@nottingham.ac.uk>
2023-01-19 03:36:54 +0000

    Remove unnecessary scripts

--- scripts/make_movies.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-declare -a StringArray=("p1_p2" "p3_p4" "p5_p6" "p7_p8" )
-
-for value in ${StringArray[@]}; do
-	ffmpeg -framerate 2 -i ${value}_%d.png -c:v libx264 -r 30 -pix_fmt yuv420p ${value}.mp4
-done;
-
