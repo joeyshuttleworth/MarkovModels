9edfabb382a8b5bdaf7bb2229c88ad2e4757d70e
Joseph <pmyjgs@nottingham.ac.uk>
2023-01-31 16:51:51 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
 
     # Required Python version
-    python_requires='==3.6',
+    python_requires='>=3.6',
 
     # List of dependencies
     install_requires=[
