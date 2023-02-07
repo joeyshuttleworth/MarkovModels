b9e74b459a0ee848e4195c3f6acd9a31e0a6eece
Joseph <pmyjgs@nottingham.ac.uk>
2023-02-07 13:45:29 +0000

    Reduce max transition rate

--- MarkovModels/common.py
+++ MarkovModels/common.py
@@ -510,7 +510,7 @@ def fit_model(mm, data, times=None, starting_parameters=None,
             rates_1 = rates_func(parameters, Vs[0])
             rates_2 = rates_func(parameters, Vs[1])
 
-            if max(rates_1.max(), rates_2.max()) > 1e7:
+            if max(rates_1.max(), rates_2.max()) > 1e5:
                 return False
 
             if min(rates_1.min(), rates_2.min()) < 1e-9:
