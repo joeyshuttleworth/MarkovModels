742b4cdd19475b4fb9037c618060e4175da8bc6d
Joseph <pmyjgs@nottingham.ac.uk>
2023-02-13 19:31:52 +0000

    Combine output and compute predictions

--- scripts/combine_fitting_results.py
+++ scripts/combine_fitting_results.py
@@ -19,15 +19,23 @@ import os
 import pandas as pd
 import numpy as np
 
+from fit_all_wells_and_protocols import compute_predictions_df
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('data_dir')
+    parser.add_argument('combine_dir')
+    parser.add_argument('data_directory')
     parser.add_argument('--output', '-o')
+    parser.add_argument('--figsize', '-f', nargs=2, type=int)
+    parser.add_argument('--model_class', default='Beattie', type=str)
+    parser.add_argument('--experiment_name', default='newtonrun4', type=str)
+    parser.add_argument('--removal_duration', type=float, default=5)
 
     args = parser.parse_args()
+    model_class = common.get_model_class(args.model_class)
 
-    glob_string = args.data_dir + r'/*/*fitted_params*csv'
+    glob_string = args.combine_dir + r'/*/*fitted_params*csv'
     print(glob_string)
     res = glob(glob_string)
     print(res)
@@ -50,6 +58,10 @@ def main():
     output_dir = common.setup_output_directory(args.output, 'combine_fitting_results')
 
     df.to_csv(os.path.join(output_dir, 'combined_fitting_results.csv'))
+
+    predictions_df = compute_predictions_df(df, output_dir, args=args,
+                                            model_class=model_class)
+    predictions_df.to_csv(os.path.join(output_dir, 'combined_predictions_df.csv'))
 
 
 if __name__ == '__main__':
