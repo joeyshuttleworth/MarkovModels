2b98ff3982199bd86acb315b9d0d88c831e7119a
Joseph <pmyjgs@nottingham.ac.uk>
2023-06-02 10:26:28 +0100

    Fix bug stopping fitting profiles from being plotted

--- MarkovModels/common.py
+++ MarkovModels/common.py
@@ -655,26 +655,26 @@ def fit_model(mm, data, times=None, starting_parameters=None,
     if output_dir:
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-            point2 = [p for i, p in enumerate(mm.get_default_parameters()) if i not
-                      in fix_parameters]
+        point2 = [p for i, p in enumerate(mm.get_default_parameters()) if i not
+                  in fix_parameters]
+        fig, axes = pints.plot.function_between_points(error,
+                                                       point_1=best_parameters,
+                                                       point_2=point2,
+                                                       padding=0.1,
+                                                       evaluations=100)
+
+        fig.savefig(os.path.join(output_dir, 'best_fitting_profile_from_default'))
+        plt.close(fig)
+
+        if randomise_initial_guess:
+            point_2 = starting_parameter_sets[best_index % len(starting_parameter_sets)]
             fig, axes = pints.plot.function_between_points(error,
                                                            point_1=best_parameters,
-                                                           point_2=point2,
+                                                           point_2=point_2,
                                                            padding=0.1,
                                                            evaluations=100)
-
-            fig.savefig(os.path.join(output_dir, 'best_fitting_profile_from_default'))
+            fig.savefig(os.path.join(output_dir, 'best_fitting_profile_from_initial_guess'))
             plt.close(fig)
-
-            if randomise_initial_guess:
-                point_2 = starting_parameter_sets[best_index % len(starting_parameter_sets)]
-                fig, axes = pints.plot.function_between_points(error,
-                                                               point_1=best_parameters,
-                                                               point_2=point_2,
-                                                               padding=0.1,
-                                                               evaluations=100)
-                fig.savefig(os.path.join(output_dir, 'best_fitting_profile_from_initial_guess'))
-                plt.close(fig)
 
     if fix_parameters:
         for i in np.unique(fix_parameters):
