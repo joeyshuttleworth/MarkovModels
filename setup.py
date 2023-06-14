d02acb2a2d3d711fff6faca9f602f0691d2aac56
Joseph <pmyjgs@nottingham.ac.uk>
2023-06-14 18:08:40 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -48,20 +48,19 @@ setup(
 
     # List of dependencies
     install_requires=[
-        'pints>=0.4.0',
-        'scipy>=1.9.1',
-        'numpy>=1.23.3',
-        'matplotlib>=3.6.2',
-        'pandas>=1.5.0',
-        'sympy<=1.11.1',
-        'numba>=0.56.2',
-        'regex>=2022.9.13',
-        'myokit>=1.33.0',
-        'seaborn>=0.12.0',
-        'markov_builder @ git+https://git@github.com/CardiacModelling/Markov-builder@master',
-        'scikit-build>=0.16.7',
-        'NumbaIDA @ git+https://git@github.com/joeyshuttleworth/NumbaIDA@master',
-        'numbalsoda @ git+https://git@github.com/NicholasWogan/NumbaLSODA@main'
+        'pints==0.4.0',
+        'scipy==1.9.1',
+        'numpy==1.23.5',
+        'matplotlib==3.6.2',
+        'pandas==1.5.0',
+        'sympy==1.11.1',
+        'numba==0.56.2',
+        'regex==2022.9.13',
+        'myokit==1.33.0',
+        'seaborn==0.12.0',
+        'markov_builder @ git+https://git@github.com/CardiacModelling/Markov-builder@8193d0f10795119e032ee9e458e15f60318594f4',
+        'scikit-build==0.16.7',
+        'numbalsoda @ git+https://git@github.com/NicholasWogan/numbalsoda@c9183b5bc75002bac5270339f0e0ca645f102b4d'
     ],
     extras_require={
         'test': [
