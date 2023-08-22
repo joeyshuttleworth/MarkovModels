008391d14899bec8b5d14d07c87452b572dfc428
Joseph <pmyjgs@nottingham.ac.uk>
2023-08-22 12:48:29 +0100

    Add test to find poorly conditioned examples of hybrid solve

--- MarkovModels/tests/test_model_generation.py
+++ MarkovModels/tests/test_model_generation.py
@@ -19,69 +19,116 @@ class TestModelGeneration(unittest.TestCase):
         self.output_dir = test_output_dir
         logging.info("outputting to " + test_output_dir)
 
-    def test_hybrid_solver(self):
+    def test_model16_solver_error(self):
 
-        model_names = [f"model{i}" for i in range(15)]
-        protocol = 'staircaseramp'
+        protocol = 'staircaseramp1'
+
         voltage_func, times, desc = common.get_ramp_protocol_from_csv(protocol)
+        model = common.make_model_of_class('model14', times, voltage_func, protocol_description=desc)
 
-        models = [common.make_model_of_class(m, times, voltage_func, protocol_description=desc)\
-                  for m in model_names]
+        full_parameters = model.get_default_parameters()
 
-        for name, model in zip(model_names, models):
-            output1 = model.make_forward_solver_current(njitted=False)()
-            output2 = model.make_hybrid_solver_current(njitted=False)()
+        boundaries = common.fitting_boundaries(full_parameters, model)
 
-            plt.plot(times, output1)
-            plt.plot(times, output2)
-            plt.savefig(os.path.join(self.output_dir, f"{name} solver comparison"))
-            plt.clf()
+        n_samples = 5000
+        sampled_parameter_sets = boundaries.sample(n=5000)
 
-            self.assertLess(np.sqrt(np.mean((output1 - output2)**2)), 1e-1)
+        hsolver = model.make_hybrid_solver_current(p_cond_threshold=np.inf)
+        solver = model.make_forward_solver_current()
 
-    def test_generated_model_output(self):
-        protocol = 'staircaseramp'
+        atol = 1e-12
+        rtol = 1e-12
 
-        for original_model, generated_model in [['Beattie', 'model3']]:
-            voltage_func, times, desc = common.get_ramp_protocol_from_csv(protocol)
+        max_error = 0
+        for i, p in enumerate(sampled_parameter_sets):
+            hres = hsolver(p, atol=atol, rtol=rtol)
+            res = solver(p, atol=atol, rtol=rtol)
 
-            model1 = common.make_model_of_class(original_model, times, voltage_func,
-                                                protocol_description=desc)
-            model2 = common.make_model_of_class(generated_model, voltage=voltage_func,
-                                                times=times, protocol_description=desc)
+            error = np.max((hres - res)**2)
+            if error > max_error:
+                max_error = error
+                max_error_index = i
 
-            default_parameters = model1.get_default_parameters()
+        # Plot max error
+        plt.plot(times,
+                 hsolver(sampled_parameter_sets[max_error_index], atol=atol, rtol=rtol) - \
+                 solver(sampled_parameter_sets[max_error_index], atol=atol, rtol=rtol),
+                 label='hybrid')
 
-            h_solver1 = model1.make_forward_solver_current()
-            h_solver2 = model2.make_forward_solver_current()
+        plt.savefig(os.path.join(self.output_dir, "model_16_max_solver_error"))
+        plt.clf()
 
-            output1 = h_solver1(default_parameters)
-            output2 = h_solver2(default_parameters)
+        plt.plot(times, solver(sampled_parameter_sets[max_error_index], atol=atol, rtol=rtol),
+                 'forward solver')
+        plt.plot(times, hsolver(sampled_parameter_sets[max_error_index], atol=atol, rtol=rtol),
+                 'hybrid solver')
 
-            rmse_error = np.sqrt(((output1 - output2)**2).mean())
+        plt.savefig(os.path.join(self.output_dir, "model_16_solver_comparison"))
+        plt.clf()
 
-            logging.debug('rmse error is: ' + f"{rmse_error}")
+    # def test_hybrid_solver(self):
 
-            plt.plot(times, output1, label=original_model)
-            plt.plot(times, output2, label='MarkovBuilder model')
-            plt.savefig(os.path.join(self.output_dir, f"{original_model}_{generated_model}_comparison_lsoda.pdf"))
-            plt.clf()
+    #     model_names = [f"model{i}" for i in range(15)]
+    #     protocol = 'staircaseramp'
+    #     voltage_func, times, desc = common.get_ramp_protocol_from_csv(protocol)
 
-            h_solver1 = model1.make_hybrid_solver_current()
-            h_solver2 = model2.make_hybrid_solver_current()
+    #     models = [common.make_model_of_class(m, times, voltage_func, protocol_description=desc)\
+    #               for m in model_names]
 
-            output1 = h_solver1(default_parameters)
-            output2 = h_solver2(default_parameters)
+    #     for name, model in zip(model_names, models):
+    #         output1 = model.make_forward_solver_current(njitted=False)()
+    #         output2 = model.make_hybrid_solver_current(njitted=False)()
 
-            rmse_error = np.sqrt(((output1 - output2)**2).mean())
+    #         plt.plot(times, output1)
+    #         plt.plot(times, output2)
+    #         plt.savefig(os.path.join(self.output_dir, f"{name} solver comparison"))
+    #         plt.clf()
 
-            logging.debug('rmse error is: ' + f"{rmse_error}")
+    #         self.assertLess(np.sqrt(np.mean((output1 - output2)**2)), 1e-1)
 
-            plt.plot(times, output1, label=original_model)
-            plt.plot(times, output2, label='MarkovBuilder model')
+    # def test_generated_model_output(self):
+    #     protocol = 'staircaseramp'
 
-            plt.savefig(os.path.join(self.output_dir, f"{original_model}_{generated_model}_comparison_hybrid.pdf"))
-            plt.clf()
+    #     for original_model, generated_model in [['Beattie', 'model3']]:
+    #         voltage_func, times, desc = common.get_ramp_protocol_from_csv(protocol)
 
-            self.assertLess(rmse_error, 1e-5)
+    #         model1 = common.make_model_of_class(original_model, times, voltage_func,
+    #                                             protocol_description=desc)
+    #         model2 = common.make_model_of_class(generated_model, voltage=voltage_func,
+    #                                             times=times, protocol_description=desc)
+
+    #         default_parameters = model1.get_default_parameters()
+
+    #         h_solver1 = model1.make_forward_solver_current()
+    #         h_solver2 = model2.make_forward_solver_current()
+
+    #         output1 = h_solver1(default_parameters)
+    #         output2 = h_solver2(default_parameters)
+
+    #         rmse_error = np.sqrt(((output1 - output2)**2).mean())
+
+    #         logging.debug('rmse error is: ' + f"{rmse_error}")
+
+    #         plt.plot(times, output1, label=original_model)
+    #         plt.plot(times, output2, label='MarkovBuilder model')
+    #         plt.savefig(os.path.join(self.output_dir, f"{original_model}_{generated_model}_comparison_lsoda.pdf"))
+    #         plt.clf()
+
+    #         h_solver1 = model1.make_hybrid_solver_current()
+    #         h_solver2 = model2.make_hybrid_solver_current()
+
+    #         output1 = h_solver1(default_parameters)
+    #         output2 = h_solver2(default_parameters)
+
+    #         rmse_error = np.sqrt(((output1 - output2)**2).mean())
+
+    #         logging.debug('rmse error is: ' + f"{rmse_error}")
+
+    #         plt.plot(times, output1, label=original_model)
+    #         plt.plot(times, output2, label='MarkovBuilder model')
+
+    #         plt.savefig(os.path.join(self.output_dir, f"{original_model}_{generated_model}_comparison_hybrid.pdf"))
+    #         plt.clf()
+
+    #         self.assertLess(rmse_error, 1e-5)
 
