347c2412e9f75595d8cf7d3b46c3bbb157757bbe
Joseph <pmyjgs@nottingham.ac.uk>
2023-08-22 12:12:26 +0100

    Fix palette selection

--- scripts/plot_subtractions_overlaid.py
+++ scripts/plot_subtractions_overlaid.py
@@ -204,7 +204,7 @@ def do_combined_plots(leak_parameters_df):
     if not os.path.exists(protocol_overlaid_dir):
         os.makedirs(protocol_overlaid_dir)
 
-    palette = sns.color_palette('husl', len(leak_parameters_df[['well', 'sweep']].unique()))
+    palette = sns.color_palette('husl', len(leak_parameters_df.groupby(['well', 'sweep']).index))
     for protocol in leak_parameters_df.protocol.unique():
         times_fname = f"{experiment_name}-{protocol}-times.csv"
         try:
@@ -244,7 +244,8 @@ def do_combined_plots(leak_parameters_df):
 
     plt.close(fig)
 
-    palette = sns.color_palette('husl', len(leak_parameters_df['protocol', 'sweep'].unique()))
+    palette = sns.color_palette('husl',
+                                len(leak_parameters_df.groupby(['protocol', 'sweep']).index))
 
     fig2 = plt.figure(figsize=args.figsize, constrained_layout=True)
     axs2 = fig2.subplots(1, 2, sharey=True)
