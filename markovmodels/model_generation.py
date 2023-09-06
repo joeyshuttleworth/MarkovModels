782309962273e9db7c59ba290dbdca74b52a39a2
Joseph <pmyjgs@nottingham.ac.uk>
2023-09-06 18:06:16 +0100

    Remove print statement

--- markovmodels/model_generation.py
+++ markovmodels/model_generation.py
@@ -25,8 +25,6 @@ def generate_markov_model_from_graph(mc: MarkovChain, times, voltage,
         raise Exception("Parameter labels length is not equal to parameter vector length"
                         f"({len(parameter_labels)} vs {len(kwargs['default_parameters'])})")
 
-    print(parameter_labels)
-
     state_labels = list(mc.graph)
 
     symbols = {}
