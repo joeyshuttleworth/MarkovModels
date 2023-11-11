8dfdff3a1bb75e1084b9c1be8a0bd9b110a7cc0f
Joseph <pmyjgs@nottingham.ac.uk>
2023-11-12 10:08:36 +1100

    Add design

--- scripts/maximise_spread_of_predictions_design.py
+++ scripts/maximise_spread_of_predictions_design.py
@@ -25,12 +25,13 @@ from markovmodels.voltage_protocols import detect_spikes, remove_spikes
 from markovmodels.fitting import infer_reversal_potential_with_artefact
 from markovmodels.utilities import get_data
 from numba import njit, jit
-from markovmodels.optimal_design import entropy_utility, D_opt_utility
+from markovmodels.optimal_design import entropy_utility, D_opt_utility, prediction_spread_utility
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('data_directory')
+    parser.add_argument('fitting_df')
     parser.add_argument('--reversal', type=float, default=-91.71)
     parser.add_argument('-o', '--output')
     parser.add_argument('--subtraction_df_file')
@@ -45,12 +46,12 @@ def main():
     parser.add_argument('--max_iterations', '-i', type=int, default=100000)
     parser.add_argument("--experiment_name", default='25112022_MW')
     parser.add_argument("--removal_duration", default=5.0, type=float)
-    parser.add_argument("--n_voxels_per_variable", type=int, default=10)
-    parser.add_argument("--n_skip", type=int, default=10)
     parser.add_argument("-c", "--no_cpus", type=int, default=1)
 
     global args
     args = parser.parse_args()
+
+    fitting_df = pd.read_csv(args.fitting_df)
 
     params_for_Erev = \
         np.loadtxt(args.artefact_default_kinetic_param_file).flatten().astype(np.float64)
@@ -94,7 +95,7 @@ def main():
         # qc_df = qc_df.set_index(['well', 'protocol', 'sweep'])
 
     # optimise one step (8th from last)
-    protocols = subtraction_df.protocol.unique()
+    protocols = fitting_df.protocol.unique()
 
     if args.protocols:
         subtraction_df = subtraction_df[(subtraction_df.protocol.isin(args.protocols))]
@@ -109,75 +110,54 @@ def main():
         subtraction_df = subtraction_df[(subtraction_df.sweep.astype(str).isin(args.sweeps))]
         qc_df = qc_df[qc_df.sweep.astype(str).isin(args.sweeps)]
 
+    # fitting_df = pd.read_csv(args.fitting_df).sort_values('score', axis=0)
     df_rows = []
     for well in passed_wells:
         for protocol in protocols:
-            for sweep in subtraction_df[(subtraction_df.well == well) & \
-                                        (subtraction_df.protocol == protocol)]['sweep'].unique():
-                leak_row = subtraction_df[(subtraction_df.well == well) &\
-                                          (subtraction_df.protocol == protocol) \
-                                          & (subtraction_df.sweep == sweep)]
+            for sweep in fitting_df[(fitting_df.well == well) & \
+                                    (fitting_df.protocol == protocol)]['sweep'].unique():
+                # leak_row = subtraction_df[(subtraction_df.well == well) &\
+                #                           (subtraction_df.protocol == protocol) \
+                #                           & (subtraction_df.sweep == sweep)]
 
-                gleak = leak_row['pre-drug leak conductance'].values[0]
-                Eleak = leak_row['pre-drug leak reversal'].values[0]
-                qc_row = qc_df[(qc_df.well == well) & (qc_df.protocol == protocol) \
-                               & (qc_df.sweep == sweep)][['Rseries', 'Cm']]
-                Rseries = qc_row['Rseries'].values[0] * 1e-9
-                Cm = qc_row['Cm'].values[0] * 1e9
-                data = get_data(well, protocol, args.data_directory,
-                                args.experiment_name, label=None, sweep=sweep)
+                # gleak = leak_row['pre-drug leak conductance'].values[0]
+                # Eleak = leak_row['pre-drug leak reversal'].values[0]
+                # qc_row = qc_df[(qc_df.well == well) & (qc_df.protocol == protocol) \
+                #                & (qc_df.sweep == sweep)][['Rseries', 'Cm']]
+                # Rseries = qc_row['Rseries'].values[0] * 1e-9
+                # Cm = qc_row['Cm'].values[0] * 1e9
+                # data = get_data(well, protocol, args.data_directory,
+                #                 args.experiment_name, label=None, sweep=sweep)
 
-                times_df = pd.read_csv(os.path.join(args.data_directory,
-                                                    f"{args.experiment_name}-{protocol}-times.csv"))
-                times = times_df['time'].to_numpy().flatten()
+                # times_df = pd.read_csv(os.path.join(args.data_directory,
+                #                                     f"{args.experiment_name}-{protocol}-times.csv"))
+                # times = times_df['time'].to_numpy().flatten()
 
-                E_obs = leak_row['fitted_E_rev'].values[0]
-                V_off = E_obs - args.reversal
-                param_set = np.concatenate((model.get_default_parameters(),
-                                           np.array([gleak, Eleak, 0, 0, V_off, Rseries, Cm])))
+                # E_obs = leak_row['fitted_E_rev'].values[0]
+                # V_off = E_obs - args.reversal
 
-                prot_func, _, desc = markovmodels.voltage_protocols.get_ramp_protocol_from_csv(protocol)
-                voltages = np.array([prot_func(t) for t in times])
+                fitted_params = fitting_df[(fitting_df.well == well)
+                                           & (fitting_df.protocol == protocol)
+                                           & (fitting_df.sweep == sweep)].head(1)[model.get_parameter_labels()].values.flatten().astype(np.float64)
+                print(fitted_params)
 
-                c_model = make_model_of_class(args.model_class, voltage=prot_func,
-                                              times=times, E_rev=args.reversal,
-                                              protocol_description=desc)
-
-                solver = c_model.make_forward_solver_current(njitted=False)
-                default_parameters = c_model.get_default_parameters()
-
-                # Remove observations within 5ms of a spike
-                removal_duration = args.removal_duration
-                spike_times, _ = detect_spikes(times, voltages, window_size=0)
-                _, _, indices = remove_spikes(times, voltages, spike_times, removal_duration)
-
-                # @njit
-                def min_func(g_kr):
-                    p = default_parameters.copy()
-                    p[-1] = g_kr
-                    return np.sum((solver(p)[indices] - data[indices]) ** 2)
-
-                # Minimise SSE to find best conductance
-                res = scipy.optimize.minimize_scalar(min_func, method='bounded', bounds=[0, 1e5])
-                gkr = res.x
-                row = [well, protocol, sweep] + list(param_set)
-                row[-8] = gkr
+                # param_set = np.concatenate((fitted_params,
+                #                            np.array([gleak, Eleak, 0, 0, V_off, Rseries, Cm])))
+                row = [well, protocol, sweep] + list(fitted_params)
                 df_rows.append(row)
 
     params = pd.DataFrame(df_rows,
                           columns=['well', 'protocol', 'sweep'] + \
-                          ArtefactModel(model).get_parameter_labels(),
-                          )
-    params = params.set_index(['well', 'protocol', 'sweep'])
+                          model.get_parameter_labels())
+    print(params)
 
-    a_model = ArtefactModel(model)
-    cfunc = a_model.get_cfunc_rhs()
+    cfunc = model.get_cfunc_rhs()
 
     x0 = markovmodels.voltage_protocols.get_design_space_representation(sc_desc).flatten()
-
     n_additional_steps = int(64 - 13 - (x0.shape[0] / 2))
-    # x0 = np.append(x0, [10.0] * 2 * n_additional_steps)
+    x0 = np.append(x0, [10] * 2 * n_additional_steps)
 
+    print('initial score: ', opt_func([x0, model, params, cfunc]))
     stds = np.empty(x0.shape)
     stds[::2] = .25 * (60 + 120)
     stds[1::2] = .25 * 1000
@@ -196,6 +176,7 @@ def main():
 
     es = cma.CMAEvolutionStrategy(x0, 1, options)
 
+    # seed = es.seed
     # with open(os.path.join('pycma_seed.txt'), 'w') as fout:
     #     fout.write(seed)
     #     fout.write('\n')
@@ -204,7 +185,7 @@ def main():
 
     while not es.stop():
         d_list = es.ask()
-        x = [(d, a_model, params, cfunc) for d in d_list]
+        x = [(d, model, params, cfunc) for d in d_list]
         res = list(map(opt_func, x))
         es.tell(d_list, res)
 
@@ -214,11 +195,10 @@ def main():
                                        parameters_to_use=model.get_parameter_labels())
 
     # Check D_optimality of design vs staircase
-    params = model.get_default_parameters()
     u_D_staircase = markovmodels.optimal_design.D_opt_utility(sc_desc,
                                                               params,
                                                               s_model,
-                                                              indices=indices)
+                                                              removal_duration=args.removal_duration)
     print(f"u_D of staircase = {u_D_staircase}")
 
     found_desc = markovmodels.voltage_protocols.design_space_to_desc(xopt)
@@ -226,7 +206,7 @@ def main():
     u_D_found = markovmodels.optimal_design.D_opt_utility(found_desc,
                                                           params,
                                                           s_model,
-                                                          indices=indices)
+                                                          removal_duration=args.removal_duration)
 
     print(f"u_D of found design = {u_D_found}")
 
@@ -257,20 +237,20 @@ def main():
     axs[1].cla()
 
     # Plot phase diagram for the new design (first two states)
-    a_model.voltage = found_voltage_func
-    a_model.protocol_description = found_desc
+    model.voltage = found_voltage_func
+    model.protocol_description = found_desc
 
-    states = a_model.make_hybrid_solver_states(njitted=False, hybrid=False)()[::args.n_skip]
+    states = model.make_hybrid_solver_states(njitted=False, hybrid=False)()[::args.n_skip]
     cols = [plt.cm.jet(i / states.shape[0]) for i in range(states.shape[0])]
     axs[0].scatter(states[:, 0], states[:, 1], alpha=.25, color=cols, marker='o')
 
     # Plot phase diagram (first two states)
-    states = a_model.make_hybrid_solver_states(njitted=False, hybrid=False)()
+    states = model.make_hybrid_solver_states(njitted=False, hybrid=False)()
     np.savetxt(os.path.join('found_design_trajectory.csv'), states)
     states = states[::args.n_skip]
 
-    a_model.voltage = sc_func
-    a_model.protocol_description = sc_desc
+    model.voltage = sc_func
+    model.protocol_description = sc_desc
     cols = [plt.cm.jet(i / states.shape[0]) for i in range(states.shape[0])]
     for i in range(states.shape[0]):
         axs[0].scatter(states[i, 0], states[i, 1], alpha=.25, color=cols, marker='o')
@@ -279,7 +259,7 @@ def main():
 
 
 def opt_func(x):
-    d, a_model, params, cfunc = x
+    d, model, params, cfunc = x
     # Force positive durations
     # x[1::2] = np.abs(x[1::2])
 
@@ -296,11 +276,23 @@ def opt_func(x):
     desc = markovmodels.voltage_protocols.design_space_to_desc(d)
 
     # ignore Vm state
-    kinetic_indices = [i for i in range(a_model.get_no_state_vars() - 1)]
-    util = entropy_utility(desc,
-                           params.values.astype(np.float64).mean(axis=0).flatten(),
-                           a_model)
-    return -util
+    kinetic_indices = [i for i in range(model.get_no_state_vars())]
+
+    utils = []
+
+    if isinstance(model, markovmodels.DisconnectedMarkovModel.DisconnectedMarkovModel):
+        cfunc = None
+
+    params = params[np.all(np.isfinite(params[model.get_parameter_labels()]), axis=1), :]
+
+    for well in params.well.unique():
+        sub_df = params[params.well == well]
+        print(sub_df)
+
+        util = prediction_spread_utility(desc, sub_df.values, model,
+                                         removal_duration=args.removal_duration)
+        utils.append(util)
+    return -np.mean(utils)
 
 
 if __name__ == '__main__':
