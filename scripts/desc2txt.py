e8e81c1b850ada101340f1f8bac806b696b50455
Joseph <pmyjgs@nottingham.ac.uk>
2023-11-13 11:10:29 +1100

    Update desc2txt to take multiple files as arguments

--- scripts/desc2txt.py
+++ scripts/desc2txt.py
@@ -7,18 +7,19 @@ from ast import literal_eval as createTuple
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('fname')
+    parser.add_argument('fnames', nargs='+')
     global args
     args = parser.parse_args()
 
-    desc = np.loadtxt(args.fname, delimiter=',')
-    output_dir = os.path.dirname(args.fname)
-
-    with open(os.path.join(output_dir, 'found_design.txt'), 'w') as fout:
-        for line in markovmodels.voltage_protocols.desc_to_table(desc)[:-1]:
-            fout.write(line)
-            print(line)
-            fout.write('\n')
+    for fname in args.fnames:
+        desc = np.loadtxt(fname, delimiter=',')
+        output_dir = os.path.dirname(fname)
+        out_fname = f"{os.path.splitext(fname)[0]}_txt.txt"
+        with open(os.path.join(output_dir, out_fname), 'w') as fout:
+            for line in markovmodels.voltage_protocols.desc_to_table(desc)[:-1]:
+                fout.write(line)
+                print(line)
+                fout.write('\n')
 
 if __name__ == '__main__':
     main()
