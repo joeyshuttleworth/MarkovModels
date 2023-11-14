eb269444f0ae7cc090d463d2b24052a77aec4b93
Joseph <pmyjgs@nottingham.ac.uk>
2023-11-14 21:15:22 +1100

    Fix typo

--- markovmodels/fitting.py
+++ markovmodels/fitting.py
@@ -342,7 +342,7 @@ def fit_well_data(model_class_name: str, well, protocol, data_directory,
             if inferred_E_rev < -50 or inferred_E_rev > -100:
                 E_rev = inferred_E_rev
 
-        except exc as exc:
+        except Exception as exc:
             print(str(exc))
             pass
 
