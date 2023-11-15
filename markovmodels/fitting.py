1ef771e1839f445d8866c337996414c06bc0aa4f
Joseph <pmyjgs@nottingham.ac.uk>
2023-11-15 12:20:04 +1100

    Move prediction functions from script to package

--- markovmodels/fitting.py
+++ markovmodels/fitting.py
@@ -10,6 +10,7 @@ import pandas as pd
 import pints
 import pints.plot
 import scipy
+import seaborn as sns
 from numba import njit
 
 import markovmodels
@@ -767,3 +768,239 @@ def infer_reversal_potential(protocol: str, current: np.array, times, ax=None,
 
     return roots[-1]
 
+
+def compute_predictions_df(params_df, output_dir, label='predictions',
+                           model_class=None, fix_EKr=None,
+                           adjust_kinetic_parameters=False,
+                           default_artefact_kinetic_parameters=None,
+                           args=None):
+
+    assert(not (fix_EKr is not None and adjust_kinetic_parameters))
+    param_labels = make_model_of_class(model_class).get_parameter_labels()
+    params_df = get_best_params(params_df, protocol_label='protocol')
+    predictions_dir = os.path.join(output_dir, label)
+
+    if not os.path.exists(predictions_dir):
+        os.makedirs(predictions_dir)
+
+    predictions_df = []
+    protocols_list = params_df['protocol'].unique()
+
+    trace_fig = plt.figure(figsize=args.figsize)
+    trace_axs = trace_fig.subplots(2)
+
+    all_models_fig = plt.figure(figsize=args.figsize)
+    all_models_axs = all_models_fig.subplots(2)
+
+    for sim_protocol in np.unique(protocols_list):
+        prot_func, times, desc = markovmodels.voltage_protocols.get_ramp_protocol_from_csv(sim_protocol)
+        full_times = pd.read_csv(
+            os.path.join(args.data_directory,
+                         f"{args.experiment_name}-{sim_protocol}-times.csv"))['time'].values.flatten()
+
+        voltages = np.array([prot_func(t) for t in full_times])
+
+        spike_times, spike_indices = markovmodels.voltage_protocols.detect_spikes(full_times, voltages,
+                                                                                  threshold=10)
+        _, _, indices = markovmodels.voltage_protocols.remove_spikes(full_times, voltages, spike_times,
+                                                   time_to_remove=args.removal_duration)
+        times = full_times[indices]
+
+        colours = sns.color_palette('husl', len(params_df['protocol'].unique()))
+
+        for well in params_df['well'].unique():
+            for predict_sweep in params_df[params_df.protocol == sim_protocol].sweep.unique():
+                try:
+                    full_data = markovmodels.utilities.get_data(well, sim_protocol,
+                                                                args.data_directory,
+                                                                experiment_name=args.experiment_name,
+                                                                label=args.data_label,
+                                                                sweep=predict_sweep)
+                except (FileNotFoundError, StopIteration) as exc:
+                    print(str(exc))
+                    continue
+
+                subdir_name = f"{well}_{sim_protocol}_sweep{predict_sweep}_predictions"\
+                    if predict_sweep is not None else f"{well}_{sim_protocol}_predictions"
+                sub_dir = os.path.join(predictions_dir, subdir_name)
+
+                if not args.use_artefact_model:
+                    E_obs = \
+                        markovmodels.infer_reversal_potential(sim_protocol,
+                                                              full_data,
+                                                              full_times,
+                                                              forward_sim_output_dir=sub_dir
+                                                              )
+
+                    model = make_model_of_class(model_class,
+                                                voltage=prot_func,
+                                                times=full_times,
+                                                E_rev=E_obs if not fix_EKr else fix_EKr,
+                                                protocol_description=desc)
+
+                    # Create dir for plot
+                else:
+                    # Use the artefact to forward simulate the voltages (using literature kinetics)
+                    model = make_model_of_class(model_class, voltage=prot_func,
+                                                times=times,
+                                                E_rev=args.reversal,
+                                                protocol_description=desc)
+
+                    model = ArtefactModel(model)
+                    forward_sim_parameters = default_artefact_kinetic_parameters.copy()
+                    param_row = params_df[(params_df.well == well) &
+                                          (params_df.protocol == sim_protocol) &\
+                                          (params_df.sweep == predict_sweep)].iloc[0]
+
+                    gleak, Eleak, V_off, Rseries, Cm = param_row[['gleak, Eleak, V_off, Rseries, Cm']]
+                    forward_sim_parameters[[-7, -6, -5, -4, -3, -2, -1]] = gleak, Eleak, 0, 0, V_off, Rseries, Cm
+                    E_obs = \
+                        infer_reversal_potential_with_artefact(sim_protocol,
+                                                               full_times,
+                                                               full_data,
+                                                               'model3',
+                                                               args.reversal,
+                                                               plot=True,
+                                                               output_path=sub_dir,
+                                                               forward_sim_output_dir=sub_dir,
+                                                               )
+                    V_off = args.reversal - E_obs
+
+                # Probably not worth compiling solver
+                solver = model.make_forward_solver_of_type(args.solver_type, njitted=False)
+                data = full_data[indices]
+
+                for i, protocol_fitted in enumerate(params_df['protocol'].unique()):
+                    print(protocol_fitted)
+                    for fitting_sweep in params_df[params_df.protocol == protocol_fitted].sweep:
+                        # Get parameters
+                        df = params_df[params_df.well == well]
+                        df = df[(df.protocol == protocol_fitted) & (df.sweep == fitting_sweep)]
+                        if df.empty:
+                            continue
+                        params = df.iloc[0][param_labels].values\
+                                                         .astype(np.float64)\
+                                                         .flatten()
+                        try:
+                            fitting_data = pd.read_csv(
+                                os.path.join(args.data_directory,
+                                             f"{args.experiment_name}-{protocol_fitted}-{well}-sweep{fitting_sweep}.csv"))
+                        except FileNotFoundError as e:
+                            print(str(e))
+                            continue
+
+                        fitting_current = fitting_data['current'].values.flatten()
+                        fitting_times = fitting_data['time'].values.flatten()
+
+                        if adjust_kinetic_parameters and not args.use_artefact_model:
+                            fitting_E_rev = markovmodels.infer_reversal_potential(protocol_fitted, fitting_current,
+                                                                                  fitting_times)
+                            if not args.reversal:
+                                Exception('reversal potential not provided')
+                            else:
+                                offset = E_obs - fitting_E_rev
+                                params[0] *= np.exp(params[1] * offset)
+                                params[2] *= np.exp(-params[3] * offset)
+                                params[4] *= np.exp(params[5] * offset)
+                                params[6] *= np.exp(-params[7] * offset)
+
+                        if not os.path.exists(sub_dir):
+                            os.makedirs(sub_dir)
+
+                        # Set V_offset
+                        if args.use_artefact_model:
+                            params[-3] = V_off
+
+                        full_prediction = solver(params)
+                        prediction = full_prediction[indices]
+
+                        score = np.sqrt(np.mean((data - prediction)**2))
+                        predictions_df.append((well, protocol_fitted,
+                                               fitting_sweep, predict_sweep, sim_protocol, score,
+                                               * params))
+
+                        if not np.all(np.isfinite(prediction)):
+                            logging.warning(f"running {sim_protocol} with parameters\
+                            from {protocol_fitted} gave non-finite values")
+                        else:
+                            # Output trace
+                            trace_axs[0].plot(full_times, full_prediction, label='prediction')
+
+                            trace_axs[1].set_xlabel("time / ms")
+                            trace_axs[0].set_ylabel("current / nA")
+                            trace_axs[0].plot(times, data, label='data', alpha=0.25, color='grey')
+                            trace_axs[0].legend()
+                            trace_axs[1].plot(full_times, voltages)
+                            trace_axs[1].set_ylabel('voltage / mV')
+                            fname = f"fitted_to_{protocol_fitted}_{fitting_sweep}.png" if protocol_fitted != sim_protocol or \
+                                fitting_sweep != predict_sweep else "fit.png"
+
+                            handles, labels = trace_axs[1].get_legend_handles_labels()
+                            by_label = dict(zip(labels, handles))
+                            plt.legend(by_label.values(), by_label.keys())
+
+                            trace_fig.savefig(os.path.join(sub_dir, fname))
+
+                            for ax in trace_axs:
+                                ax.cla()
+
+                            all_models_axs[0].plot(full_times, full_prediction,
+                                                   label=f"{protocol_fitted}_{fitting_sweep}", color=colours[i])
+
+                all_models_axs[1].set_xlabel("time / ms")
+                all_models_axs[0].set_ylabel("current / nA")
+                all_models_axs[0].plot(times, data, color='grey', alpha=0.5, label='data')
+                # all_models_axs[0].legend()
+                all_models_axs[0].set_title(f"{well} {sim_protocol} fits comparison")
+                all_models_axs[0].set_ylabel("Current / nA")
+
+                all_models_axs[1].plot(full_times, voltages)
+                all_models_axs[1].set_ylabel('voltage / mV')
+
+                all_models_fig.savefig(os.path.join(sub_dir, "all_fits.png"))
+
+                for ax in all_models_axs:
+                    ax.cla()
+
+    predictions_df = pd.DataFrame(np.array(predictions_df), columns=['well',
+                                                                     'fitting_protocol',
+                                                                     'fitting_sweep',
+                                                                     'prediction_sweep',
+                                                                     'validation_protocol',
+                                                                     'score'] +
+                                  param_labels)
+    predictions_df['RMSE'] = predictions_df['score']
+    predictions_df['sweep'] = predictions_df.fitting_sweep
+
+    plt.close(trace_fig)
+    plt.close(all_models_fig)
+
+    return predictions_df
+
+
+def get_best_params(fitting_df, protocol_label='protocol'):
+    best_params = []
+
+    # Ensure score is a float - may be read from csv file
+    fitting_df['score'] = fitting_df['score'].astype(np.float64)
+    fitting_df = fitting_df[np.isfinite(fitting_df['score'])].copy()
+
+    if 'sweep' not in fitting_df.columns:
+        fitting_df['sweep'] = -1
+
+    for protocol in fitting_df[protocol_label].unique():
+        for well in fitting_df['well'].unique():
+            for sweep in fitting_df['sweep'].unique():
+                sub_df = fitting_df[(fitting_df['well'] == well)
+                                    & (fitting_df[protocol_label] == protocol)].copy()
+                sub_df = sub_df[sub_df.sweep == sweep]
+                sub_df = sub_df.dropna()
+                # Get index of min score
+                if len(sub_df.index) == 0:
+                    continue
+                best_params.append(sub_df[sub_df.score == sub_df.score.min()].head(1).copy())
+
+    if not best_params:
+        raise Exception()
+
+    return pd.concat(best_params, ignore_index=True)
