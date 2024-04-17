a09d1d152541216c0c5e1f31c84b2e9f664714c6
Joseph <pmyjgs@nottingham.ac.uk>
2024-04-17 15:54:55 +0100

    Plot data on optimisation plot

--- scripts/thesis/chapter4/optimisation_results.py
+++ scripts/thesis/chapter4/optimisation_results.py
@@ -318,6 +318,7 @@ def do_trace_plots(current_ax, protocol_ax, protocol, well, sweep, params_df, ar
                            current, voltages, label=args.data_label)
 
     current_ax.plot(times*1e-3, pred)
+    current_ax.plot(times*1e-3, trace, color='grey', alpha=.5)
     current_ax.set_xlabel('')
     current_ax.set_ylabel(r'$I_\text{subtracted}$ (pA)')
     protocol_ax.set_ylabel(r'$V_\text{m}$ (mV)')
