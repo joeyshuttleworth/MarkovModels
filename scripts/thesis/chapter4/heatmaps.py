3719207016dc53278db93d5fdbf6ef822da9f188
Joseph <pmyjgs@nottingham.ac.uk>
2024-05-08 23:04:04 +0100

    Fix heatmap ordering

--- scripts/thesis/chapter4/heatmaps.py
+++ scripts/thesis/chapter4/heatmaps.py
@@ -405,6 +405,9 @@ def do_heatmap(ax, model_class, fitting_case, params_df, subtraction_df,
     pivot_df.dropna(axis=0, inplace=True, how='all')
     pivot_df.dropna(axis=1, inplace=True, how='all')
 
+    pivot_df = pivot_df.reindex([p for p in protocol_order if p in pivot_df.columns,
+                                 axis=1)
+
     if 'cbar' not in kws:
         kws['cbar'] = False
         if 'cbar_ax' in kws:
