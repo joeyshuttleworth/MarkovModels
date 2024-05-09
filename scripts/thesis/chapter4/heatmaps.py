d873dca3a3324fa6d9b1b9473120ea40334c46dc
Joseph <pmyjgs@nottingham.ac.uk>
2024-05-09 11:45:25 +0100

    Handle empty dataframes

--- scripts/thesis/chapter4/heatmaps.py
+++ scripts/thesis/chapter4/heatmaps.py
@@ -408,6 +408,9 @@ def do_heatmap(ax, model_class, fitting_case, params_df, subtraction_df,
     pivot_df = pivot_df.reindex([p for p in protocol_order if p in pivot_df.columns],
                                  axis=1)
 
+    if pivot_df.shape == 0:
+        return None
+
     if 'cbar' not in kws:
         kws['cbar'] = False
         if 'cbar_ax' in kws:
