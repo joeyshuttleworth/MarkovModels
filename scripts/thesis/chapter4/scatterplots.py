73ce8a84a2b8290bf325cd768ecc0bb95931a45c
Joseph <pmyjgs@nottingham.ac.uk>
2024-05-13 17:37:18 +0100

    Fix scatterplots

--- scripts/thesis/chapter4/scatterplots.py
+++ scripts/thesis/chapter4/scatterplots.py
@@ -423,10 +423,10 @@ def do_per_plots(protocol, well, params_df, p1, p2, output_dir, beta=None,
                 protocol_only_effect = mean_well_effect + protocol_effect
 
                 if args.log_a:
-                    protocol_effect = inverse_log_transform(protocol_only_effect,
+                    protocol_only_effect = inverse_log_transform(protocol_only_effect,
                                                             p1, p2)
 
-                ax.scatter(*(protocol_effect).T, color='gold', marker='s')
+                ax.scatter(*(protocol_only_effect).T, color='gold', marker='s')
 
 
             combined_effect = well_effect + protocol_effect
