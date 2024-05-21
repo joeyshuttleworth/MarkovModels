7027eb9a930a148aca60d392b737bddfd975ff4d
Joseph <pmyjgs@nottingham.ac.uk>
2024-05-21 13:17:38 +0100

    Fix heatmap scores

--- scripts/thesis/chapter4/heatmaps.py
+++ scripts/thesis/chapter4/heatmaps.py
@@ -442,7 +442,7 @@ def do_heatmap(ax, model_class, fitting_case, params_df, subtraction_df,
     mean_training_score = sub_df[sub_df.fitting_protocol == sub_df.validation_protocol]['n_score'].values.astype(np.float64).mean()
     mean_validation_score = sub_df[sub_df.fitting_protocol != sub_df.validation_protocol]['n_score'].values.astype(np.float64).mean()
     ax.set_title(r'$\mathcal{E}_\text{train} = $' f"{mean_training_score:.2E}" + \
-    r'$\mathcal{E})\text{predict} = $' + f"{mean_validation_score:.2E}",
+    r', $\mathcal{E})\text{predict} = $' + f"{mean_validation_score:.2E}",
     fontsize=8)
 
     hm = sns.heatmap(pivot_df, ax=ax, square=True, norm=norm,
