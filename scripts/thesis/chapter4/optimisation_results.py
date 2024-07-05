2618fcde8d87fa5f5c8053d7068c351df7a9dcd1
Joseph <pmyjgs@nottingham.ac.uk>
2024-07-05 15:43:10 +0100

    Remove unecessary JIT compilation

--- scripts/thesis/chapter4/optimisation_results.py
+++ scripts/thesis/chapter4/optimisation_results.py
@@ -534,7 +534,6 @@ def do_profile_plots(baseline_profile_ax, params_df, protocol, well, sweep, args
     default_params = m_model.get_default_parameters()
     solver = m_model.make_hybrid_solver_current(hybrid=False)
 
-    @njit
     def compute_rmse(p):
         if np.any(p <= 0):
             return np.nan
