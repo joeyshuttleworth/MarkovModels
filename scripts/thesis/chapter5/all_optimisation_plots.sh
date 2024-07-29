3ce540a44e13d778abea385d0e45c1e6d40fade4
Joseph <pmyjgs@nottingham.ac.uk>
2024-07-29 10:55:26 +0100

    Fix script

--- scripts/thesis/chapter5/all_optimisation_plots.sh
+++ scripts/thesis/chapter5/all_optimisation_plots.sh
@@ -6,6 +6,7 @@ do
     for case in ${cases[@]};
         do
             echo ${case} ${model}
-            python3 scripts/thesis/chapter4/optimisation_results.py thesis_data/25112022_MW_FF_processed/traces ${case} thesis_data/sydney_fitting/25112022MW/${case}/${model}/combine_fitting_results/combined>
+	          python3 scripts/thesis/chapter4/optimisation_results.py thesis_data/25112022_MW_FF_processed/traces ${case} thesis_data/sydney_fitting/25112022MW/${case}/${model}/combine_fitting_results/combined_fitting_results.csv thesis_data/25112022_MW_FF_processed/subtraction_qc.csv ${model} --output thesis_plots/chapter4/optimisation/${model}_${case} -c 20 --experiment_name 25112022_MW --plot_wip
         done;
     done;
+
