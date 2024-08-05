85c777635264043d88951b95e2c305a5e3aa52c0
Joseph <pmyjgs@nottingham.ac.uk>
2024-08-05 10:55:46 +0100

    Fix scatterplots

--- scripts/thesis/chapter4/scatterplots.py
+++ scripts/thesis/chapter4/scatterplots.py
@@ -397,8 +397,10 @@ def do_per_plots(protocol, well, params_df, p1, p2, output_dir, beta=None,
                 ax.set_yscale('log')
 
     for ax in axs[::2]:
-        ax.set_xlabel(f"{convert_to_latex(p1)} ({units[p1]})")
         ax.set_ylabel(f"{convert_to_latex(p2)} ({units[p2]})")
+
+    for ax in axs.flatten()[-2:]
+        ax.set_xlabel(f"{convert_to_latex(p1)} ({units[p1]})")
 
     if all_data_ax:
         all_data_ax.set_xlabel(f"{convert_to_latex(p1)} ({units[p1]})")
