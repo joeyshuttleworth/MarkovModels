bab8bd4c9afd22e408265c14922d3648b193cf3c
Joseph <pmyjgs@nottingham.ac.uk>
2024-08-07 23:22:34 +0100

    Fix debug output

--- markovmodels/fitting.py
+++ markovmodels/fitting.py
@@ -1744,7 +1744,7 @@ def make_prediction(model_class, args, well, sim_protocol, predict_sweep,
             artefact_params = np.full(no_artefact_parameters, np.nan)
 
         if not np.all(np.isfinite(artefact_params)):
-            logging.warning(f"{fitting_case} {model_class} {well} {protocol} {sweep} Got non-finite artefact parameters: {artefact_params}")
+            logging.warning(f"{fitting_case} {model_class} {well} {fitting_protocol} {sweep} Got non-finite artefact parameters: {artefact_params}")
             artefact_params = np.full(no_artefact_parameters, np.nan)
 
         if not np.all(np.isfinite(artefact_params)):
