27c8a51e6079b58444241102250c9b105832f97d
Joseph <pmyjgs@nottingham.ac.uk>
2024-08-13 14:42:53 +0100

    Modify heatmaps

--- scripts/thesis/chapter4/heatmaps.py
+++ scripts/thesis/chapter4/heatmaps.py
@@ -428,7 +428,7 @@ def main():
 
         best_well_title = f"{best_well} " + '\n' \
             + r'$\mathcal{E}_{\mathrm{fit}} = $' f"{mean_training_score:.2f}" + \
-            ',\n' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}"
+            r'\,,' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}"
 
         best_ax.set_title(best_well_title, fontsize=args.fontsize)
 
@@ -441,7 +441,7 @@ def main():
                                               ]['n_score'].values.astype(np.float64).mean()
         worst_well_title = f"{worst_well} " + '\n' \
             + r'$\mathcal{E}_{\mathrm{fit}} = $' f"{mean_training_score:.2f}" + \
-            ',\n' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}"
+            r'\,,' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}"
 
         worst_ax.set_title(worst_well_title, fontsize=args.fontsize)
         # worst_ax.axis('off')
@@ -542,7 +542,7 @@ def main():
 
         model_name = relabel_models_dict[model_class]
         ax.set_title(r'\textbf{' + model_name + r'}' + "\n"+ r'$\mathcal{E}_{\mathrm{fit}} = $' f"{mean_training_score:.2f}" + \
-                     ",\n" r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}",
+                     r',\,' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}",
                      fontsize=args.fontsize)
 
 
@@ -962,7 +962,7 @@ def do_heatmap(ax, model_class, fitting_case, params_df, subtraction_df,
     mean_validation_score = sub_df[sub_df.fitting_protocol != sub_df.validation_protocol]['n_score'].values.astype(np.float64).mean()
 
     ax.set_title(r'$\mathcal{E}_{\mathrm{fit}} = $' f"{mean_training_score:.2f}" + \
-    ",\n" r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}")
+    r'\,,' r'$\mathcal{E}_{\mathrm{predict}} = $' + f"{mean_validation_score:.2f}")
 
     hm = sns.heatmap(pivot_df, ax=ax, square=True, norm=norm,
                      cmap=cmap, **kws)
