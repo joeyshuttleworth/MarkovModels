13e4295e7657ea4e43013cab518db782e18ddeb2
Joseph <pmyjgs@nottingham.ac.uk>
2024-08-13 19:09:17 +0100

    Fix prediction comparison plot

--- scripts/thesis/chapter4/prediction_comparison.py
+++ scripts/thesis/chapter4/prediction_comparison.py
@@ -15,6 +15,8 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.colors import SymLogNorm
 from matplotlib import rc
 
+from cycler import cycler
+
 import markovmodels
 from markovmodels.model_generation import make_model_of_class
 from markovmodels.fitting import get_best_params, make_prediction, adjust_kinetics, get_ensemble_of_predictions
@@ -72,13 +74,13 @@ def main():
     parser.add_argument('subtraction_df')
     parser.add_argument('--fitting_cases', nargs='+', default=['0a', '0b', '0c',
                                                                'II', '0d'])
+    parser.add_argument('--well', '-w', default='B20')
     parser.add_argument('--model_class', default='model3')
     parser.add_argument('--plot_all_predictions', action='store_true')
     parser.add_argument('--data_label', default='')
     parser.add_argument('--ignore_protocols', nargs='+', default=['longap'], type=str)
     parser.add_argument('--ignore_wells', nargs='+', default=['M06'], type=str)
     parser.add_argument('--vlims', nargs=2, type=float, default=(-100, 100))
-    parser.add_argument('-w', '--wells', type=str, nargs='+', default=[])
     parser.add_argument('--removal_duration', type=float, default=5.0)
     parser.add_argument('--experiment_name', '-e', default='newtonrun4')
     parser.add_argument('--validation_protocols', nargs='+', default=['longap'])
@@ -86,7 +88,7 @@ def main():
     parser.add_argument('--fig_title', '-t', default='')
     parser.add_argument('--nolegend', action='store_true')
     parser.add_argument('--dpi', '-d', default=500, type=int)
-    parser.add_argument('--fontsize', type=int, default=12)
+    parser.add_argument('--fontsize', type=int, default=8)
     parser.add_argument('--show_uncertainty', action='store_true')
     parser.add_argument('--shared_plot_limits', action='store_true')
     parser.add_argument('--file_format', default='')
@@ -116,22 +118,10 @@ def main():
     params_df = pd.read_csv(fname)
     subtraction_df = pd.read_csv(args.subtraction_df)
 
-    protocol_dict = {}
     v_func = None
     sweep = 0
-    # prediction_df = compute_predictions_df(params_df, output_dir,
-    #                                        protocol_dict, case,
-    #                                        args.reversal, subtraction_df,
-    #                                        model_class=args.model_class,
-    #                                        args=args,
-    #                                        label=f"{args.model_class}_{case}_predictions",
-    #                                        data_label=data_label,
-    #                                        hybrid=False,
-    #                                        strict=False,
-    #                                        )
 
-    # best_well = prediction_df.groupby('well').agg(agg_dict).idxmin()['n_score']
-    best_well = 'B20'
+    best_well = 'C12'
 
     cases = ['0a', '0b', '0c']
     dirnames_dict = { '0a': 'Case0a',
@@ -139,6 +129,34 @@ def main():
                       '0c': 'Case0b'}
 
     dirnames = [dirnames_dict[case] for case in cases]
+
+    fitting_case = '0b'
+    model_class = 'model3'
+
+    if not args.well:
+        protocol_dict = {}
+        for protocol in sorted(list(params_df.protocol) + args.validation_protocols):
+            times = np.loadtxt(os.path.join(args.data_directory,
+                                            f"{args.experiment_name}-{protocol}-times.csv")).astype(np.float64).flatten()
+            v_func, desc = get_ramp_protocol_from_json(protocol,
+                                                    os.path.join(args.data_directory,
+                                                                    'protocols'),
+                                                    args.experiment_name)
+            protocol_dict[protocol] = desc, times
+
+
+        prediction_df = compute_predictions_df(params_df, output_dir,
+                                               protocol_dict, fitting_case,
+                                               args.reversal, subtraction_df,
+                                               model_class=model_class,
+                                               hybrid=False,
+                                               strict=False,
+                                               plot=False,
+                                               args=args)
+
+        best_well = prediction_df.groupby('well').agg(agg_dict).idxmin()['n_score']
+    else:
+        best_well = args.well
 
     model_classes = ['model2', 'model3', 'model10', 'Wang']
     # Get fitting results (dict of dicts)
@@ -174,6 +192,7 @@ def main():
 
     protocols = list(np.unique(list(itertools.chain(*[list(params_df.protocol.unique()) for params_df in params_dfs])))) + args.validation_protocols
 
+    protocol_dict = {}
     for protocol in protocols:
         v_func, desc = get_ramp_protocol_from_json(protocol, os.path.join(args.data_directory, 'protocols'),
                                               args.experiment_name)
@@ -183,9 +202,20 @@ def main():
 
         protocol_dict[protocol] = desc, times
 
-    fig = plt.figure(figsize=args.figsize)
-    axs = fig.subplots(3, 2, sharex=True, height_ratios=[0.3, 1, 1])
+    fig = plt.figure(figsize=args.figsize, constrained_layout=True)
+    axs = fig.subplots(3, 2, sharex=False,
+                       height_ratios=[0.3, 1, 1])
 
+    for ax in axs.flatten():
+        ax.spines[['top', 'right']].set_visible(False)
+
+    for ax in axs[1:, 0].flatten():
+        ax.set_ylabel(r'$I_{\mathrm{Kr}}$ (pA)')
+
+    for ax in axs[-1, :].flatten():
+        ax.set_xlabel(r'$t$ (ms)')
+
+    axs[0, 0].set_ylabel(r'$V_\mathrm{m}$ (mV)')
 
     # Staircase protocol, predictions from each model
     protocol = 'staircaseramp1'
@@ -201,14 +231,16 @@ def main():
                                     f"{args.experiment_name}-{protocol}-times.csv")).astype(np.float64).flatten()
 
     Vcmd = np.array([v_func(t) for t in times])
-    axs[0, 0].plot(times, Vcmd, color='black')
-    axs[0, 0].plot(times, best_data, color='grey', alpha=.25)
+    axs[0, 0].plot(times*1e-3, Vcmd, color='black', lw=.75)
+    axs[1, 0].plot(times*1e-3, best_data, color='grey', alpha=.5, lw=.5)
 
     axs[0, 0].set_title(r'$d_1^{(1)}$')
 
-    for ax, cap in zip(axs.flatten(), ['a', 'b', 'c', 'd', 'e', 'f']):
+    for ax, cap in zip(axs.T.flatten(), ['a', 'b', 'c', 'd', 'e', 'f']):
         ax.set_title(cap, loc='left', fontweight='bold')
 
+    linestyle_cycler = ['-', '--', '-.', ':', 'solid',
+                        'dashed', 'dashdot', 'dotted']
 
     model_label_dict = {
         'model2': 'C-O-I',
@@ -218,9 +250,8 @@ def main():
     }
 
     case = '0b'
-    for model_class in model_classes:
+    for i, model_class in enumerate(model_classes):
         params_df = params_df_dict[(model_class, case)]
-        print(params_df.columns)
         pred = make_prediction(model_class, args, best_well,
                                protocol, sweep,
                                protocol, sweep, params_df,
@@ -229,30 +260,129 @@ def main():
                                best_data, Vcmd,
                                label=data_label)
 
-        axs[1, 0].plot(times, pred, label=model_label_dict[model])
+        axs[1, 0].plot(times*1e-3, pred, label=model_label_dict[model_class],
+                       ls=linestyle_cycler[i], lw=.75, alpha=.5)
+    # axs[1, 0].legend()
 
-    axs[1, 0].legend()
+    model_class = 'model3'
+    case = '0b'
 
-
-    # Model 3 prediction of all wells
-    params_df = params_df_dict[('model3', '0b')]
-
-    for well in params_df.well.unique():
+    for i, well in enumerate(sorted(list(params_df.well.unique()))):
         data, vp = get_data(best_well, protocol,
                             args.data_directory, args.experiment_name, sweep=sweep)
 
-        pred = make_prediction(model_class, args, best_well,
+        params_df = params_df_dict[(model_class, case)]
+        pred = make_prediction(model_class, args, well,
                                protocol, sweep,
                                protocol, sweep, params_df,
                                subtraction_df, case,
                                args.reversal, protocol_dict,
                                data, Vcmd,
                                label=data_label)
-        axs[2, 0].plot(times, pred, label='well')
+        axs[2, 0].plot(times*1e-3, pred, label=well, ls=linestyle_cycler[i],
+                       lw=.75, alpha=.5)
 
-    axs[2, 0].legend(ncol=5)
+    # axs[2, 0].legend(ncol=2)
+
+    for ax in axs.flatten():
+        ax.spines[['top', 'right']].set_visible(False)
+
+    for ax in axs[1:, 0].flatten():
+        ax.set_ylabel(r'$I_{\mathrm{Kr}}$ (pA)')
+
+    for ax in axs[-1, :].flatten():
+        ax.set_xlabel(r'$t$ (ms)')
+
+    axs[0, 0].set_ylabel(r'$V_\mathrm{m}$ (mV)')
+
+    # longap protocol, predictions from each model's staircase estimates
+    protocol = 'longap'
+    sweep = 0
+
+    v_func, desc = get_ramp_protocol_from_json(protocol,
+                                               os.path.join(args.data_directory, 'protocols'),
+                                               args.experiment_name)
+    best_data, vp = get_data(best_well, protocol,
+                             args.data_directory, args.experiment_name, sweep=sweep)
+
+    times = np.loadtxt(os.path.join(args.data_directory,
+                                    f"{args.experiment_name}-{protocol}-times.csv")).astype(np.float64).flatten()
+
+    Vcmd = np.array([v_func(t) for t in times])
+    axs[0, 1].plot(times*1e-3, Vcmd, color='black', lw=.75)
+    axs[1, 1].plot(times*1e-3, best_data, color='grey', alpha=.5, lw=.5)
+
+    axs[0, 1].set_title(r'$d_6$')
+
+    fitting_protocol = 'staircaseramp1'
+
+    for ax, cap in zip(axs.T.flatten(), ['a', 'b', 'c', 'd', 'e', 'f']):
+        ax.set_title(cap, loc='left', fontweight='bold')
+
+    linestyle_cycler = ['-', '--', '-.', ':', 'solid',
+                        'dashed', 'dashdot', 'dotted']
+
+    model_label_dict = {
+        'model2': 'C-O-I',
+        'model3': 'Beattie',
+        'model10': 'Kemp',
+        'Wang': 'Wang'
+    }
+
+    case = '0b'
+    for i, model_class in enumerate(model_classes):
+        params_df = params_df_dict[(model_class, case)]
+        pred = make_prediction(model_class, args, best_well,
+                               protocol, sweep,
+                               fitting_protocol, sweep, params_df,
+                               subtraction_df, case,
+                               args.reversal, protocol_dict,
+                               best_data, Vcmd,
+                               label=data_label)
+
+        axs[1, 1].plot(times*1e-3, pred, label=model_label_dict[model_class],
+                       ls=linestyle_cycler[i], lw=.75, alpha=.5)
+
+    model_class = 'model3'
+    case = '0b'
+
+    for i, well in enumerate(sorted(list(params_df.well.unique()))):
+        data, vp = get_data(best_well, protocol,
+                            args.data_directory, args.experiment_name, sweep=sweep)
+
+        params_df = params_df_dict[(model_class, case)]
+        pred = make_prediction(model_class, args, well,
+                               protocol, sweep,
+                               fitting_protocol, sweep, params_df,
+                               subtraction_df, case,
+                               args.reversal, protocol_dict,
+                               data, Vcmd,
+                               label=data_label)
+
+        axs[2, 1].plot(times*1e-3, pred, label=well, ls=linestyle_cycler[i],
+                       lw=.75, alpha=.5)
+
+    ylims = np.vstack([ax.get_ylim() for ax in axs[1:, 0].flatten()])
+    for ax in axs[1:, 0]:
+        ax.set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
+
+    ylims = np.vstack([ax.get_ylim() for ax in axs[1:, 1].flatten()])
+    for ax in axs[1:, 1]:
+        ax.set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
+
+    axs[1, 0].legend(fontsize=8)
+    axs[2, 0].legend(ncol=3, fontsize=8)
+
+
+    for ax in axs[:-1, :].flatten():
+        ax.set_xticklabels([])
 
     fig.savefig(os.path.join(output_dir, "prediction_comparison"))
+
+    for ax in axs[:-1, :].flatten():
+        ax.set_xticklabels([])
+
+    plt.close(fig)
 
 
 if __name__ == '__main__':
