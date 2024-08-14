d39bf2c8dbd0553820ea038d1683e9a7ba935ad8
Joseph <pmyjgs@nottingham.ac.uk>
2024-08-14 16:53:40 +0100

    Fix prediction comparison

--- scripts/thesis/chapter4/prediction_comparison.py
+++ scripts/thesis/chapter4/prediction_comparison.py
@@ -279,6 +279,9 @@ def main():
                                args.reversal, protocol_dict,
                                data, Vcmd,
                                label=data_label)
+        # Normalise prediction
+        pred = pred / np.sqrt(np.mean(pred**2))
+
         axs[2, 0].plot(times*1e-3, pred, label=well, ls=linestyle_cycler[i],
                        lw=.75, alpha=.5)
 
@@ -287,8 +290,10 @@ def main():
     for ax in axs.flatten():
         ax.spines[['top', 'right']].set_visible(False)
 
-    for ax in axs[1:, 0].flatten():
-        ax.set_ylabel(r'$I_{\mathrm{Kr}}$ (pA)')
+    # for ax in axs[1:, 0].flatten():
+    #     ax.set_ylabel(r'$I_{\mathrm{Kr}}$ (pA)')
+    axs[1, 0].set_ylabel(r'$I_{\mathrm{Kr}}$ (pA)')
+    axs[2, 0].set_ylabel(r'$I_{\mathrm{Kr}}$ (normalised)')
 
     for ax in axs[-1, :].flatten():
         ax.set_xlabel(r'$t$ (ms)')
@@ -359,19 +364,21 @@ def main():
                                data, Vcmd,
                                label=data_label)
 
+        # Normalise prediction
+        pred = pred / np.sqrt(np.mean(pred**2))
+
         axs[2, 1].plot(times*1e-3, pred, label=well, ls=linestyle_cycler[i],
                        lw=.75, alpha=.5)
 
     ylims = np.vstack([ax.get_ylim() for ax in axs[1:, 0].flatten()])
-    for ax in axs[1:, 0]:
-        ax.set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
+
+    axs[1, 0].set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
 
     ylims = np.vstack([ax.get_ylim() for ax in axs[1:, 1].flatten()])
-    for ax in axs[1:, 1]:
-        ax.set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
+    axs[1, 1].set_ylim(ylims[:, 0].min(), ylims[:, 1].max())
 
     axs[1, 0].legend(fontsize=8)
-    axs[2, 0].legend(ncol=3, fontsize=8)
+    axs[2, 0].legend(ncol=2, fontsize=8)
 
 
     for ax in axs[:-1, :].flatten():
