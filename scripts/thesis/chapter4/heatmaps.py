2f05eaa503b275152fe49731b458efc4cfc06be5
Joseph <pmyjgs@nottingham.ac.uk>
2024-09-08 17:42:04 +0100

    Fix protocol labelling

--- scripts/thesis/chapter4/heatmaps.py
+++ scripts/thesis/chapter4/heatmaps.py
@@ -847,8 +847,8 @@ def get_protocol_label(protocol_order, protocol, sweep):
                       if prot not in ['staircaseramp1_sweep2',
                                       'staircaseramp1_2_sweep2']]
 
-    prot_index = protocol_order.index(protocol)
-    ret_str = r'$d_{' + str(protocol_order.index(protocol) + 1) \
+    prot_index = protocol_order.index(p_protocol)
+    ret_str = r'$d_{' + str(prot_index + 1) \
         + r'}'
     if p_protocol == 'staircaseramp1':
         ret_str += r'^{(' + str(sweep) + r')}$'
